f1657c814a4c0fbbb70e23a6365ad3233ef622ef
to-omer <toomerhs@gmail.com>
2020-04-26 12:14:56 +0900

    first commt

---
+++ src/tools/input.rs
@@ -0,0 +1,78 @@
+use cargo_snippet::snippet;
+
+#[macro_export(local_inner_macros)]
+#[snippet("input")]
+macro_rules! read_value {
+    ($iter:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($iter, $t)),* )
+    };
+    ($iter:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+    };
+    ($iter:expr, { chars: $base:expr }) => {
+        read_value!($iter, String).chars().map(|c| (c as u8 - $base as u8) as usize).collect::<Vec<usize>>()
+    };
+    ($iter:expr, { char: $base:expr }) => {
+        read_value!($iter, { chars: $base })[0]
+    };
+    ($iter:expr, chars) => {
+        read_value!($iter, String).chars().collect::<Vec<char>>()
+    };
+    ($iter:expr, char) => {
+        read_value!($iter, chars)[0]
+    };
+    ($iter:expr, usize1) => {
+        read_value!($iter, usize) - 1
+    };
+    ($iter:expr, $t:ty) => {
+        $iter.next().unwrap().parse::<$t>().unwrap()
+    };
+}
+#[macro_export(local_inner_macros)]
+#[snippet("input")]
+macro_rules! input_inner {
+    ($iter:expr) => {};
+    ($iter:expr, ) => {};
+    ($iter:expr, mut $var:ident : $t:tt $($r:tt)*) => {
+        let mut $var = read_value!($iter, $t);
+        input_inner!{$iter $($r)*}
+    };
+    ($iter:expr, mut $var:ident $($r:tt)*) => {
+        input_inner!{$iter, mut $var : usize $($r)*}
+    };
+    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($iter, $t);
+        input_inner!{$iter $($r)*}
+    };
+    ($iter:expr, $var:ident $($r:tt)*) => {
+        input_inner!{$iter, $var : usize $($r)*}
+    };
+}
+#[snippet("input")]
+#[macro_export(local_inner_macros)]
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+    (iter = $iter:ident, $($r:tt)*) => {
+        let s = {
+            use std::io::Read;
+            let mut s = String::new();
+            std::io::stdin().read_to_string(&mut s).unwrap();
+            s
+        };
+        let mut $iter = s.split_whitespace();
+        input_inner!{$iter, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let s = {
+            use std::io::Read;
+            let mut s = String::new();
+            std::io::stdin().read_to_string(&mut s).unwrap();
+            s
+        };
+        let mut iter = s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+}
