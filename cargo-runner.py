cb876ba81adbc2e5ded7b7ccd03a0a9f27040d86
to-omer <toomerhs@gmail.com>
2020-04-26 16:35:33 +0900

    verify tet

---
+++ cargo-runner.py
@@ -0,0 +1,16 @@
+import sys
+import subprocess
+
+if __name__ == "__main__":
+    bindatas = toml.load("Cargo.toml").get("bin")
+    names = dict()
+    if bindatas is not None:
+        for bindata in bindatas:
+            name = bindata.get("name")
+            path = bindata.get("path")
+            if path is not None:
+                names[path] = name
+
+        subprocess.check_call(["cargo", "--release", "--quiet", "--bin", names[sys.argv[1]]])
+
+    assert False
