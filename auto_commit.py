8041dc5f2eb55b01abb47f05968dda8bf3844ce3
to-omer <toomerhs@gmail.com>
2020-05-17 12:02:05 +0900

    fix workflow

--- auto_commit.py
+++
@@ -1,9 +0,0 @@
-import os
-from subprocess import check_call
-
-url=f'https://{os.environ['GITHUB_ACTOR']}:{os.environ['GITHUB_TOKEN']}@github.com/{os.environ['GITHUB_REPOSITORY']}.git'
-check_call(['git', 'config', '--local', 'user.name', 'GitHub'])
-check_call(['git', 'config', '--local', 'user.email', 'noreply@github.com'])
-message=f'[auto-verifier] verify commit {os.environ['GITHUB_SHA']}'
-check_call(['git', 'commit', '-m', message, '-a'])
-check_call(['git', 'push', url, 'HEAD'])
