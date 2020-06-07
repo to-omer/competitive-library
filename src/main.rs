befcd29aa374a2611f5ded1beea1bbe086def4f1
to-omer <toomerhs@gmail.com>
2020-06-07 11:33:22 +0900

    add output template

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,34 @@ macro_rules! input {
 
 #[cargo_snippet::snippet]
 fn main() {
+    #[allow(unused_imports)]
+    use std::io::Write as _;
+    let __out = std::io::stdout();
+    #[allow(unused_mut, unused_variables)]
+    let mut __out = std::io::BufWriter::new(__out.lock());
+    #[allow(unused_macros)]
+    macro_rules! print {
+        ($($arg:tt)*) => (std::write!(__out, $($arg)*).unwrap())
+    }
+    #[allow(unused_macros)]
+    macro_rules! println {
+        ($($arg:tt)*) => (std::writeln!(__out, $($arg)*).unwrap())
+    }
+    #[allow(unused_macros)]
+    macro_rules! echo {
+        ($iter:expr) => {
+            echo!($iter, "\n")
+        };
+        ($iter:expr, $sep:expr) => {
+            let mut iter = $iter;
+            if let Some(item) = iter.next() {
+                print!("{}", item);
+            }
+            for item in iter {
+                print!("{}{}", $sep, item);
+            }
+            println!();
+        };
+    }
     input!(n);
 }
