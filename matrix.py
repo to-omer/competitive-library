34f49f59e3d0ed68c80faf5328a8e1a97499d507
to-omer <toomerhs@gmail.com>
2020-07-18 00:47:36 +0900

    update action

---
+++ matrix.py
@@ -0,0 +1,31 @@
+from argparse import ArgumentParser
+from subprocess import Popen, PIPE, call
+from pathlib import Path
+from shutil import move
+
+def cargo_verify(name: str):
+    return ['cargo', 'test', '--lib', '--release', name, '--', '--ignored']
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument('size', type=int, default=20)
+    parser.add_argument('nth', type=int, nargs='?')
+    args = parser.parse_args()
+    if args.nth is None:
+        print(f'::set-output name=matrix::{{"job-id":{list(range(args.size))}}}')
+        return
+
+    with Popen(['cargo', 'test', '--lib', '--quiet', '--release', '--', '--list', '--ignored'], stdout=PIPE) as p:
+        for s in p.stdout.readlines()[args.nth::args.size]:
+            call(cargo_verify(s.split()[0][:-1].decode('utf-8')))
+
+    with Popen(['git', 'ls-files', '-o', '--exclude-standard', 'src'], stdout=PIPE) as p:
+        artifact = Path('artifact')
+        artifact.mkdir(exist_ok=True)
+        for s in p.stdout.read().split():
+            target = artifact / Path(s.decode('utf-8'))
+            target.parent.mkdir(parents=True, exist_ok=True)
+            move(s.decode('utf-8'), target)
+
+if __name__ == '__main__':
+    main()
