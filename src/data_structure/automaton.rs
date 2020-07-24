417d54de69ef51d76b95afb67ee542e05644589d
to-omer <toomerhs@gmail.com>
2020-07-24 18:21:03 +0900

    add automaton

---
+++ src/data_structure/automaton.rs
@@ -0,0 +1,243 @@
+use crate::algebra::Monoid;
+
+#[cargo_snippet::snippet("automaton")]
+pub trait Automaton {
+    type Alphabet;
+    type State;
+
+    fn initial(&self) -> Self::State;
+    fn next(&self, state: &Self::State, alph: &Self::Alphabet) -> Option<Self::State>;
+    fn accept(&self, state: &Self::State) -> bool;
+}
+
+#[cargo_snippet::snippet("automaton")]
+pub fn automaton_dp<A, M>(
+    dfa: A,
+    sigma: impl Iterator<Item = A::Alphabet> + Clone,
+    len: usize,
+    monoid: M,
+    mul: impl Fn(&M::T, &A::Alphabet) -> M::T,
+    init: M::T,
+) -> M::T
+where
+    A: Automaton,
+    A::State: Eq + std::hash::Hash,
+    M: Monoid,
+{
+    let mut dp = std::collections::HashMap::new();
+    let mut ndp = std::collections::HashMap::new();
+    dp.insert(dfa.initial(), init);
+    for _ in 0..len {
+        for (state, value) in dp.drain() {
+            for alph in sigma.clone() {
+                if let Some(nstate) = dfa.next(&state, &alph) {
+                    let nvalue = mul(&value, &alph);
+                    ndp.entry(nstate)
+                        .and_modify(|acc| *acc = monoid.operate(acc, &value))
+                        .or_insert(nvalue);
+                }
+            }
+        }
+        std::mem::swap(&mut dp, &mut ndp);
+        ndp.clear();
+    }
+    let mut acc = monoid.unit();
+    for (state, value) in dp.into_iter() {
+        if dfa.accept(&state) {
+            acc = monoid.operate(&acc, &value);
+        }
+    }
+    acc
+}
+
+#[cargo_snippet::snippet("automaton")]
+pub struct IntersectionAutomaton<X: Automaton, Y: Automaton>(X, Y);
+#[cargo_snippet::snippet("automaton")]
+impl<A, X, Y> Automaton for IntersectionAutomaton<X, Y>
+where
+    X: Automaton<Alphabet = A>,
+    Y: Automaton<Alphabet = A>,
+{
+    type Alphabet = A;
+    type State = (X::State, Y::State);
+    fn initial(&self) -> Self::State {
+        (self.0.initial(), self.1.initial())
+    }
+    fn next(&self, state: &Self::State, alph: &Self::Alphabet) -> Option<Self::State> {
+        match (self.0.next(&state.0, alph), self.1.next(&state.1, alph)) {
+            (Some(s0), Some(s1)) => Some((s0, s1)),
+            _ => None,
+        }
+    }
+    fn accept(&self, state: &Self::State) -> bool {
+        self.0.accept(&state.0) && self.1.accept(&state.1)
+    }
+}
+
+#[cargo_snippet::snippet("automaton")]
+pub struct UnionAutomaton<X: Automaton, Y: Automaton>(X, Y);
+#[cargo_snippet::snippet("automaton")]
+impl<A, X, Y> Automaton for UnionAutomaton<X, Y>
+where
+    X: Automaton<Alphabet = A>,
+    Y: Automaton<Alphabet = A>,
+{
+    type Alphabet = A;
+    type State = (X::State, Y::State);
+    fn initial(&self) -> Self::State {
+        (self.0.initial(), self.1.initial())
+    }
+    fn next(&self, state: &Self::State, alph: &Self::Alphabet) -> Option<Self::State> {
+        match (self.0.next(&state.0, alph), self.1.next(&state.1, alph)) {
+            (Some(s0), Some(s1)) => Some((s0, s1)),
+            _ => None,
+        }
+    }
+    fn accept(&self, state: &Self::State) -> bool {
+        self.0.accept(&state.0) || self.1.accept(&state.1)
+    }
+}
+
+#[cargo_snippet::snippet("automaton")]
+pub struct ProductAutomaton<X: Automaton, Y: Automaton>(X, Y);
+#[cargo_snippet::snippet("automaton")]
+impl<X: Automaton, Y: Automaton> Automaton for ProductAutomaton<X, Y> {
+    type Alphabet = (X::Alphabet, Y::Alphabet);
+    type State = (X::State, Y::State);
+    fn initial(&self) -> Self::State {
+        (self.0.initial(), self.1.initial())
+    }
+    fn next(&self, state: &Self::State, alph: &Self::Alphabet) -> Option<Self::State> {
+        match (
+            self.0.next(&state.0, &alph.0),
+            self.1.next(&state.1, &alph.1),
+        ) {
+            (Some(s0), Some(s1)) => Some((s0, s1)),
+            _ => None,
+        }
+    }
+    fn accept(&self, state: &Self::State) -> bool {
+        self.0.accept(&state.0) && self.1.accept(&state.1)
+    }
+}
+
+#[cargo_snippet::snippet("automaton")]
+pub struct LessThanAutomaton<'a, T: Ord> {
+    buf: &'a [T],
+    eq: bool,
+}
+#[cargo_snippet::snippet("automaton")]
+impl<'a, T: Ord> LessThanAutomaton<'a, T> {
+    pub fn new(buf: &'a [T], eq: bool) -> Self {
+        Self { buf, eq }
+    }
+}
+#[cargo_snippet::snippet("automaton")]
+impl<T: Ord> Automaton for LessThanAutomaton<'_, T> {
+    type Alphabet = T;
+    type State = (usize, bool);
+    fn initial(&self) -> Self::State {
+        (0, true)
+    }
+    fn next(&self, state: &Self::State, alph: &Self::Alphabet) -> Option<Self::State> {
+        self.buf
+            .get(state.0)
+            .and_then(|c| match (state.1, c.cmp(alph)) {
+                (true, std::cmp::Ordering::Equal) => Some((state.0 + 1, true)),
+                (true, std::cmp::Ordering::Less) => None,
+                _ => Some((state.0 + 1, false)),
+            })
+    }
+    fn accept(&self, state: &Self::State) -> bool {
+        self.eq || !state.1
+    }
+}
+
+#[cargo_snippet::snippet("automaton")]
+pub struct GreaterThanAutomaton<'a, T: Ord> {
+    buf: &'a [T],
+    eq: bool,
+}
+#[cargo_snippet::snippet("automaton")]
+impl<'a, T: Ord> GreaterThanAutomaton<'a, T> {
+    pub fn new(buf: &'a [T], eq: bool) -> Self {
+        Self { buf, eq }
+    }
+}
+#[cargo_snippet::snippet("automaton")]
+impl<T: Ord> Automaton for GreaterThanAutomaton<'_, T> {
+    type Alphabet = T;
+    type State = (usize, bool);
+    fn initial(&self) -> Self::State {
+        (0, true)
+    }
+    fn next(&self, state: &Self::State, alph: &Self::Alphabet) -> Option<Self::State> {
+        self.buf
+            .get(state.0)
+            .and_then(|c| match (state.1, c.cmp(alph)) {
+                (true, std::cmp::Ordering::Equal) => Some((state.0 + 1, true)),
+                (true, std::cmp::Ordering::Greater) => None,
+                _ => Some((state.0 + 1, false)),
+            })
+    }
+    fn accept(&self, state: &Self::State) -> bool {
+        self.eq || !state.1
+    }
+}
+
+#[cargo_snippet::snippet("automaton")]
+pub struct ContainAutomaton<'a, T: Eq>(&'a T);
+#[cargo_snippet::snippet("automaton")]
+impl<'a, T: Eq> Automaton for ContainAutomaton<'a, T> {
+    type Alphabet = T;
+    type State = bool;
+    fn initial(&self) -> Self::State {
+        false
+    }
+    fn next(&self, state: &Self::State, alph: &Self::Alphabet) -> Option<Self::State> {
+        Some(*state || self.0 == alph)
+    }
+    fn accept(&self, state: &Self::State) -> bool {
+        *state
+    }
+}
+
+#[cargo_snippet::snippet("automaton")]
+pub struct ContainCounterAutomaton<'a, T: Eq>(&'a T);
+#[cargo_snippet::snippet("automaton")]
+impl<'a, T: Eq> Automaton for ContainCounterAutomaton<'a, T> {
+    type Alphabet = T;
+    type State = usize;
+    fn initial(&self) -> Self::State {
+        0
+    }
+    fn next(&self, state: &Self::State, alph: &Self::Alphabet) -> Option<Self::State> {
+        Some(*state + (self.0 == alph) as usize)
+    }
+    fn accept(&self, state: &Self::State) -> bool {
+        *state > 0
+    }
+}
+
+#[cargo_snippet::snippet("automaton")]
+pub struct AlwaysAcceptingAutomaton<A>(std::marker::PhantomData<fn() -> A>);
+#[cargo_snippet::snippet("automaton")]
+impl<A> AlwaysAcceptingAutomaton<A> {
+    pub fn new() -> Self {
+        Self(std::marker::PhantomData)
+    }
+}
+#[cargo_snippet::snippet("automaton")]
+impl<A> Automaton for AlwaysAcceptingAutomaton<A> {
+    type Alphabet = A;
+    type State = ();
+    fn initial(&self) -> Self::State {
+        ()
+    }
+    fn next(&self, _state: &Self::State, _alph: &Self::Alphabet) -> Option<Self::State> {
+        Some(())
+    }
+    fn accept(&self, _state: &Self::State) -> bool {
+        true
+    }
+}
--- src/data_structure/mod.rs
+++ src/data_structure/mod.rs
@@ -1,5 +1,6 @@
 //! data structures
 
+mod automaton;
 mod binary_indexed_tree;
 mod bitset;
 mod disjoint_sparse_table;
@@ -8,6 +9,7 @@ mod segment_tree;
 mod sliding_winsow_aggregation;
 mod union_find;
 
+pub use automaton::*;
 pub use binary_indexed_tree::{BinaryIndexedTree, BinaryIndexedTree2D};
 pub use bitset::BitSet;
 pub use disjoint_sparse_table::DisjointSparseTable;
