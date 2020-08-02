2c4640305da7b4305b07c2222b9200b0d4dfb057
to-omer <toomerhs@gmail.com>
2020-08-03 00:23:01 +0900

    add mo solver

---
+++ src/algorithm/mo.rs
@@ -0,0 +1,33 @@
+#[cargo_snippet::snippet]
+pub trait MoSolver {
+    type Answer;
+    fn insert(&mut self, i: usize);
+    fn remove(&mut self, i: usize);
+    fn answer(&mut self) -> Self::Answer;
+    fn mo_solve(&mut self, sqn: usize, ans: &mut [Self::Answer], lr: &[(usize, usize)]) {
+        let q = lr.len();
+        let mut idx: Vec<usize> = (0..q).collect();
+        idx.sort_by_key(|&i| (lr[i].0 / sqn, lr[i].1));
+        let (mut nl, mut nr) = (0, 0);
+        for &i in idx.iter() {
+            let (l, r) = lr[i];
+            while nl > l {
+                nl -= 1;
+                self.insert(nl);
+            }
+            while nr < r {
+                self.insert(nr);
+                nr += 1;
+            }
+            while nl < l {
+                self.remove(nl);
+                nl += 1;
+            }
+            while nr > r {
+                nr -= 1;
+                self.remove(nr);
+            }
+            ans[i] = self.answer();
+        }
+    }
+}
--- src/algorithm/mod.rs
+++ src/algorithm/mod.rs
@@ -2,12 +2,14 @@
 
 mod compress;
 mod convex_hull_trick;
+mod mo;
 mod search;
 mod slide_minimum;
 mod zeta_transform;
 
 pub use compress::Compress;
 pub use convex_hull_trick::*;
+pub use mo::MoSolver;
 pub use search::*;
 pub use slide_minimum::slide_minimum;
 pub use zeta_transform::*;
