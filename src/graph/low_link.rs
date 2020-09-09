48093c49303e9ec9ce3ff3a6266550510d63a74c
to-omer <toomerhs@gmail.com>
2020-09-09 18:17:59 +0900

    fix low_link

--- src/graph/low_link.rs
+++ src/graph/low_link.rs
@@ -1,42 +1,42 @@
 use super::Graph;
 
 #[cargo_snippet::snippet("LowLink")]
-pub struct LowLink {
-    pub ord: Vec<usize>,
+pub struct LowLink<'a> {
+    graph: &'a Graph,
     pub low: Vec<usize>,
+    pub ord: Vec<usize>,
     pub articulation: Vec<usize>,
     pub bridge: Vec<(usize, usize)>,
 }
 #[cargo_snippet::snippet("LowLink")]
-impl LowLink {
-    pub fn new(graph: &Graph) -> Self {
+impl<'a> LowLink<'a> {
+    pub fn new(graph: &'a Graph) -> Self {
         let mut self_ = Self {
-            ord: vec![0; graph.vsize],
+            graph,
             low: vec![0; graph.vsize],
+            ord: vec![std::usize::MAX; graph.vsize],
             articulation: vec![],
             bridge: vec![],
         };
-        let mut visited = vec![false; graph.vsize];
         for u in graph.vertices() {
-            if !visited[u] {
-                self_.dfs(u, graph.vsize, &mut 0, &mut visited, &graph);
+            if self_.ord[u] == std::usize::MAX {
+                self_.dfs(u, graph.vsize, &mut 0);
             }
         }
         self_
     }
-    fn dfs(&mut self, u: usize, p: usize, i: &mut usize, visited: &mut Vec<bool>, graph: &Graph) {
-        visited[u] = true;
-        self.ord[u] = *i;
-        self.low[u] = *i;
-        *i += 1;
+    fn dfs(&mut self, u: usize, p: usize, now_ord: &mut usize) {
+        self.low[u] = *now_ord;
+        self.ord[u] = *now_ord;
+        *now_ord += 1;
         let mut is_articulation = false;
         let mut cnt = 0;
-        for a in graph.adjacency(u) {
-            if !visited[a.to] {
+        for a in self.graph.adjacency(u) {
+            if self.ord[a.to] == std::usize::MAX {
                 cnt += 1;
-                self.dfs(a.to, u, i, visited, graph);
+                self.dfs(a.to, u, now_ord);
                 self.low[u] = self.low[u].min(self.low[a.to]);
-                is_articulation |= p < graph.vsize && self.ord[u] <= self.low[a.to];
+                is_articulation |= p < self.graph.vsize && self.ord[u] <= self.low[a.to];
                 if self.ord[u] < self.low[a.to] {
                     self.bridge.push((u.min(a.to), u.max(a.to)));
                 }
@@ -44,7 +44,7 @@ impl LowLink {
                 self.low[u] = self.low[u].min(self.ord[a.to]);
             }
         }
-        is_articulation |= p == graph.vsize && cnt > 1;
+        is_articulation |= p == self.graph.vsize && cnt > 1;
         if is_articulation {
             self.articulation.push(u);
         }
