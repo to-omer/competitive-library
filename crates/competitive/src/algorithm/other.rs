c566b55e288ca88f22595f7be2554e4ce85150c4
to-omer <toomerhs@gmail.com>
2021-02-23 00:55:10 +0900

    add floor_kernel

--- crates/competitive/src/algorithm/other.rs
+++ crates/competitive/src/algorithm/other.rs
@@ -38,3 +38,44 @@ where
     let looped = path.split_off(loop_start);
     (path, looped)
 }
+
+#[codesnip::entry]
+/// $y = \left\lfloor\frac{n}{x}\right\rfloor$
+///
+/// segments that have same x or y
+pub fn floor_kernel(n: usize) -> Vec<usize> {
+    let m = (n as f64).sqrt() as usize;
+    let mut res = Vec::with_capacity(m * 2 + 1);
+    for i in 1..=m {
+        res.push(i);
+    }
+    if n / m + 1 != m + 1 {
+        res.push(m + 1);
+    }
+    for i in (1..=m).rev() {
+        res.push(n / i + 1);
+    }
+    res
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_floor_kernel() {
+        for n in 1..1000 {
+            let k = floor_kernel(n);
+            let from = k.iter().cloned().zip(k.iter().cloned().skip(1));
+            let to = k.iter().cloned().zip(k.iter().cloned().skip(1)).rev();
+            for ((a, b), (c, d)) in from.zip(to) {
+                assert!(a < b);
+                assert!(c < d);
+                for x in a..b {
+                    for y in c..d {
+                        assert!(x * y <= n);
+                    }
+                }
+            }
+        }
+    }
+}
