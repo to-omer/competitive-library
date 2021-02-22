6be897edc7f630f1193a23274d60cf74c13bbc05
to-omer <toomerhs@gmail.com>
2021-02-23 01:07:05 +0900

    add segment tree map

--- crates/competitive/src/data_structure/lazy_segment_tree.rs
+++ crates/competitive/src/data_structure/lazy_segment_tree.rs
@@ -218,3 +218,208 @@ fn test_lazy_segment_tree() {
         }
     }
 }
+
+#[codesnip::entry("LazySegmentTreeMap", include("algebra"))]
+#[derive(Clone, Debug)]
+pub struct LazySegmentTreeMap<M: Monoid, E: Monoid, F: Fn(&M::T, &E::T) -> M::T> {
+    n: usize,
+    height: u32,
+    seg: std::collections::HashMap<usize, (M::T, E::T)>,
+    m: M,
+    e: E,
+    f: F,
+}
+#[codesnip::entry("LazySegmentTreeMap")]
+impl<M: Monoid, E: Monoid, F: Fn(&M::T, &E::T) -> M::T> LazySegmentTreeMap<M, E, F> {
+    pub fn new(n: usize, m: M, e: E, f: F) -> Self {
+        Self {
+            n: n.next_power_of_two(),
+            height: n.next_power_of_two().trailing_zeros(),
+            seg: Default::default(),
+            m,
+            e,
+            f,
+        }
+    }
+    #[inline]
+    fn propagate(&mut self, k: usize) {
+        debug_assert!(k < self.n);
+        let x = self
+            .seg
+            .get(&k)
+            .map(|t| t.1.clone())
+            .unwrap_or_else(|| self.e.unit());
+        if x != self.e.unit() {
+            let tl = self
+                .seg
+                .entry(2 * k)
+                .or_insert((self.m.unit(), self.e.unit()));
+            tl.1 = self.e.operate(&tl.1, &x);
+            let tr = self
+                .seg
+                .entry(2 * k + 1)
+                .or_insert((self.m.unit(), self.e.unit()));
+            tr.1 = self.e.operate(&tr.1, &x);
+            *self.seg.entry(k).or_insert((self.m.unit(), self.e.unit())) =
+                (self.reflect(k), self.e.unit());
+        }
+    }
+    #[inline]
+    fn thrust(&mut self, k: usize) {
+        for i in (1..=self.height).rev() {
+            self.propagate(k >> i);
+        }
+    }
+    #[inline]
+    fn reflect(&self, k: usize) -> M::T {
+        let u = (self.m.unit(), self.e.unit());
+        let t = self.seg.get(&k).unwrap_or(&u);
+        if t.1 != self.e.unit() {
+            (self.f)(&t.0, &t.1)
+        } else {
+            t.0.clone()
+        }
+    }
+    #[inline]
+    fn recalc(&mut self, mut k: usize) {
+        k /= 2;
+        while k > 0 {
+            self.seg
+                .entry(k)
+                .or_insert((self.m.unit(), self.e.unit()))
+                .0 = self
+                .m
+                .operate(&self.reflect(2 * k), &self.reflect(2 * k + 1));
+            k /= 2;
+        }
+    }
+    pub fn update(&mut self, l: usize, r: usize, x: E::T) {
+        debug_assert!(l < self.n);
+        debug_assert!(r <= self.n);
+        let mut a = l + self.n;
+        let mut b = r + self.n;
+        self.thrust(a);
+        self.thrust(b - 1);
+        while a < b {
+            if a & 1 != 0 {
+                let t = self.seg.entry(a).or_insert((self.m.unit(), self.e.unit()));
+                t.1 = self.e.operate(&t.1, &x);
+                a += 1;
+            }
+            if b & 1 != 0 {
+                b -= 1;
+                let t = self.seg.entry(b).or_insert((self.m.unit(), self.e.unit()));
+                t.1 = self.e.operate(&t.1, &x);
+            }
+            a /= 2;
+            b /= 2;
+        }
+        self.recalc(l + self.n);
+        self.recalc(r + self.n - 1);
+    }
+    pub fn fold(&mut self, l: usize, r: usize) -> M::T {
+        debug_assert!(l < self.n);
+        debug_assert!(r <= self.n);
+        let mut l = l + self.n;
+        let mut r = r + self.n;
+        self.thrust(l);
+        self.thrust(r - 1);
+        let mut vl = self.m.unit();
+        let mut vr = self.m.unit();
+        while l < r {
+            if l & 1 != 0 {
+                vl = self.m.operate(&vl, &self.reflect(l));
+                l += 1;
+            }
+            if r & 1 != 0 {
+                r -= 1;
+                vr = self.m.operate(&self.reflect(r), &vr);
+            }
+            l /= 2;
+            r /= 2;
+        }
+        self.m.operate(&vl, &vr)
+    }
+    pub fn set(&mut self, k: usize, x: M::T) {
+        let k = k + self.n;
+        self.thrust(k);
+        *self.seg.entry(k).or_insert((self.m.unit(), self.e.unit())) = (x, self.e.unit());
+        self.recalc(k);
+    }
+    pub fn get(&mut self, k: usize) -> M::T {
+        self.fold(k, k + 1)
+    }
+    pub fn fold_all(&mut self) -> M::T {
+        self.fold(0, self.n)
+    }
+}
+
+#[test]
+fn test_lazy_segment_tree_map() {
+    use crate::algebra::{AdditiveOperation, CartesianOperation, LastOperation, MaxOperation};
+    use crate::tools::Xorshift;
+    let mut rand = Xorshift::time();
+    let n = 1_024;
+    let m = 20_000;
+    // Range Sum Query & Range Add Query
+    let mut seg = LazySegmentTreeMap::new(
+        n,
+        CartesianOperation::new(AdditiveOperation::new(), AdditiveOperation::new()),
+        AdditiveOperation::new(),
+        |x, &y| (x.0 + y * x.1, x.1),
+    );
+    for i in 0..n {
+        seg.set(i, (0, 1));
+    }
+    let mut arr = vec![0; n];
+    for _ in 0..m {
+        let q = rand.rand(2);
+        if q == 0 {
+            // Range Add Query
+            let l = rand.rand(n as u64) as usize;
+            let r = rand.rand((n - l + 1) as u64) as usize + l;
+            let x = rand.rand(1_000_000_000) as usize;
+            seg.update(l, r, x);
+            for a in arr[l..r].iter_mut() {
+                *a += x;
+            }
+        } else {
+            // Range Sum Query
+            let l = rand.rand(n as u64) as usize;
+            let r = rand.rand((n - l + 1) as u64) as usize + l;
+            let mut res = 0;
+            for a in arr[l..r].iter() {
+                res += a;
+            }
+            assert_eq!(seg.fold(l, r).0, res);
+        }
+    }
+
+    // Range Max Query & Range Update Query
+    let mut seg = LazySegmentTree::new(n, MaxOperation::new(), LastOperation::new(), |&x, y| {
+        y.unwrap_or(x)
+    });
+    let mut arr = vec![0; n];
+    for _ in 0..m {
+        let q = rand.rand(2);
+        if q == 0 {
+            // Range Update Query
+            let l = rand.rand(n as u64) as usize;
+            let r = rand.rand((n - l + 1) as u64) as usize + l;
+            let x = rand.rand(1_000_000_000) as usize;
+            seg.update(l, r, Some(x));
+            for a in arr[l..r].iter_mut() {
+                *a = x;
+            }
+        } else {
+            // Range Max Query
+            let l = rand.rand(n as u64) as usize;
+            let r = rand.rand((n - l + 1) as u64) as usize + l;
+            let mut res = 0;
+            for a in arr[l..r].iter() {
+                res = std::cmp::max(res, *a);
+            }
+            assert_eq!(seg.fold(l, r), res);
+        }
+    }
+}
--- crates/competitive/src/data_structure/segment_tree.rs
+++ crates/competitive/src/data_structure/segment_tree.rs
@@ -1,3 +1,5 @@
+#![allow(clippy::or_fun_call)]
+
 use crate::algebra::Monoid;
 
 #[codesnip::entry("SegmentTree", include("algebra"))]
@@ -134,6 +136,197 @@ fn test_segment_tree() {
     let n = 1_024;
     let q = 10_000;
     let mut seg = SegmentTree::new(n, AdditiveOperation::new());
+    let mut arr = vec![0; n + 1];
+    for _ in 0..q {
+        let k = rand.rand(n as u64) as usize;
+        let v = rand.rand(1_000_000_000) as usize;
+        seg.set(k, v);
+        arr[k + 1] = v;
+    }
+    for i in 0..n {
+        arr[i + 1] += arr[i];
+    }
+    for i in 0..n {
+        for j in i + 1..n + 1 {
+            assert_eq!(seg.fold(i, j), arr[j] - arr[i]);
+        }
+    }
+    for _ in 0..q {
+        let v = rand.rand(1_000_000_000 * n as u64) as usize;
+        assert_eq!(
+            seg.lower_bound_all(|&x| v <= x, n),
+            arr[1..].position_bisect(|&x| x >= v)
+        );
+    }
+    for _ in 0..q {
+        let v = rand.rand(1_000_000_000 * n as u64) as usize;
+        let mut l = rand.rand(n as u64) as usize;
+        let mut r = rand.rand(n as u64) as usize;
+        if l > r {
+            std::mem::swap(&mut l, &mut r);
+        }
+        assert_eq!(
+            seg.lower_bound(|&x| v <= x, l, r),
+            arr[l + 1..r + 1].position_bisect(|&x| x >= v + arr[l]) + l
+        );
+    }
+
+    let n = 1_000;
+    let mut seg = SegmentTree::new(n, MaxOperation::new());
+    let mut arr = vec![0; n];
+    for _ in 0..q {
+        let k = rand.rand(n as u64) as usize;
+        let v = rand.rand(1_000_000_000) as usize;
+        seg.set(k, v);
+        arr[k] = v;
+    }
+    for _ in 0..n {
+        let l = rand.rand(n as u64) as usize;
+        let r = rand.rand((n - l + 1) as u64) as usize + l;
+        let mut res = 0;
+        for a in arr[l..r].iter().cloned() {
+            res = std::cmp::max(res, a);
+        }
+        assert_eq!(seg.fold(l, r), res);
+    }
+}
+
+#[codesnip::entry("SegmentTreeMap", include("algebra"))]
+#[derive(Clone, Debug)]
+pub struct SegmentTreeMap<M: Monoid> {
+    n: usize,
+    seg: std::collections::HashMap<usize, M::T>,
+    m: M,
+    u: M::T,
+}
+#[codesnip::entry("SegmentTreeMap")]
+impl<M: Monoid> SegmentTreeMap<M> {
+    pub fn new(n: usize, m: M) -> Self {
+        let n = n.next_power_of_two();
+        let u = m.unit();
+        Self {
+            n,
+            seg: Default::default(),
+            m,
+            u,
+        }
+    }
+    #[inline]
+    fn get_ref(&self, k: usize) -> &M::T {
+        self.seg.get(&k).unwrap_or(&self.u)
+    }
+    pub fn set(&mut self, k: usize, x: M::T) {
+        debug_assert!(k < self.n);
+        let mut k = k + self.n;
+        *self.seg.entry(k).or_insert(self.m.unit()) = x;
+        k /= 2;
+        while k > 0 {
+            *self.seg.entry(k).or_insert(self.m.unit()) =
+                self.m.operate(self.get_ref(2 * k), self.get_ref(2 * k + 1));
+            k /= 2;
+        }
+    }
+    pub fn update(&mut self, k: usize, x: M::T) {
+        debug_assert!(k < self.n);
+        let mut k = k + self.n;
+        let t = self.seg.entry(k).or_insert(self.m.unit());
+        *t = self.m.operate(&t, &x);
+        k /= 2;
+        while k > 0 {
+            *self.seg.entry(k).or_insert(self.m.unit()) =
+                self.m.operate(self.get_ref(2 * k), self.get_ref(2 * k + 1));
+            k /= 2;
+        }
+    }
+    pub fn get(&self, k: usize) -> M::T {
+        debug_assert!(k < self.n);
+        self.seg
+            .get(&(k + self.n))
+            .cloned()
+            .unwrap_or_else(|| self.m.unit())
+    }
+    pub fn fold(&self, l: usize, r: usize) -> M::T {
+        debug_assert!(l < self.n);
+        debug_assert!(r <= self.n);
+        let mut l = l + self.n;
+        let mut r = r + self.n;
+        let mut vl = self.m.unit();
+        let mut vr = self.m.unit();
+        while l < r {
+            if l & 1 != 0 {
+                vl = self.m.operate(&vl, self.get_ref(l));
+                l += 1;
+            }
+            if r & 1 != 0 {
+                r -= 1;
+                vr = self.m.operate(self.get_ref(r), &vr);
+            }
+            l /= 2;
+            r /= 2;
+        }
+        self.m.operate(&vl, &vr)
+    }
+    pub fn fold_all(&self) -> M::T {
+        self.seg.get(&1).cloned().unwrap_or_else(|| self.m.unit())
+    }
+    /// left most index [0, r) that satisfies monotonic condition
+    pub fn lower_bound_all<F: Fn(&M::T) -> bool>(&self, f: F, r: usize) -> usize {
+        if !f(&self.get_ref(1)) {
+            return r;
+        }
+        let mut acc = self.m.unit();
+        let mut pos = 1;
+        while pos < self.n {
+            pos *= 2;
+            let y = self.m.operate(&acc, self.get_ref(pos));
+            if !f(&y) {
+                acc = y;
+                pos += 1;
+            }
+        }
+        std::cmp::min(pos - self.n, r)
+    }
+    /// left most index [l, r) that satisfies monotonic condition
+    pub fn lower_bound<F: Fn(&M::T) -> bool>(&self, f: F, l: usize, r: usize) -> usize {
+        let mut acc = self.m.unit();
+        let mut pos = l + self.n;
+        let mut lim = r + self.n;
+        loop {
+            let y = self.m.operate(&acc, self.get_ref(pos));
+            if f(&y) {
+                while pos < self.n {
+                    pos *= 2;
+                    let y = self.m.operate(&acc, self.get_ref(pos));
+                    if !f(&y) {
+                        acc = y;
+                        pos += 1;
+                    }
+                }
+                return std::cmp::min(pos - self.n, r);
+            }
+            let is_right = pos & 1 == 1;
+            if pos == lim {
+                return r;
+            }
+            pos /= 2;
+            lim /= 2;
+            if is_right {
+                acc = y;
+                pos += 1;
+            }
+        }
+    }
+}
+
+#[test]
+fn test_segment_tree_map() {
+    use crate::algebra::{AdditiveOperation, MaxOperation};
+    use crate::algorithm::SliceBisectExt;
+    use crate::tools::Xorshift;
+    let mut rand = Xorshift::time();
+    let n = 1_024;
+    let q = 10_000;
+    let mut seg = SegmentTreeMap::new(n, AdditiveOperation::new());
     let mut arr = vec![0; n + 1];
     for _ in 0..q {
         let k = rand.rand(n as u64) as usize;
