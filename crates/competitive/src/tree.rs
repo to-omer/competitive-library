ee57667fb77a8f52fb91172b3e3415a8d290a1c5
to-omer <toomerhs@gmail.com>
2021-05-07 01:45:21 +0900

    add tree_dp

--- crates/competitive/src/tree.rs
+++ crates/competitive/src/tree.rs
@@ -10,6 +10,7 @@ mod generator;
 mod heavy_light_decomposition;
 mod rerooting;
 mod tree_center;
+mod tree_dp;
 mod tree_hash;
 mod tree_order;
 mod tree_rec;
---
+++ crates/competitive/src/tree/tree_dp.rs
@@ -0,0 +1,43 @@
+use crate::graph::UndirectedSparseGraph;
+
+#[codesnip::entry("tree_dp", include("SparseGraph"))]
+impl UndirectedSparseGraph {
+    pub fn tree_dp_bottom_up<T, F>(&self, root: usize, dp: &mut [T], mut f: F)
+    where
+        F: FnMut(&mut T, &T),
+    {
+        fn dfs<T, F>(g: &UndirectedSparseGraph, u: usize, p: usize, dp: &mut [T], f: &mut F)
+        where
+            F: FnMut(&mut T, &T),
+        {
+            for a in g.adjacencies(u) {
+                if a.to != p {
+                    dfs(g, a.to, u, dp, f);
+                    assert_ne!(u, a.to);
+                    let ptr = dp.as_mut_ptr();
+                    unsafe { f(&mut *ptr.add(u), &*ptr.add(a.to)) };
+                }
+            }
+        }
+        dfs(self, root, !0, dp, &mut f);
+    }
+    pub fn tree_dp_top_down<T, F>(&self, root: usize, dp: &mut [T], mut f: F)
+    where
+        F: FnMut(&mut T, &T),
+    {
+        fn dfs<T, F>(g: &UndirectedSparseGraph, u: usize, p: usize, dp: &mut [T], f: &mut F)
+        where
+            F: FnMut(&mut T, &T),
+        {
+            for a in g.adjacencies(u) {
+                if a.to != p {
+                    assert_ne!(u, a.to);
+                    let ptr = dp.as_mut_ptr();
+                    unsafe { f(&mut *ptr.add(a.to), &*ptr.add(u)) };
+                    dfs(g, a.to, u, dp, f);
+                }
+            }
+        }
+        dfs(self, root, !0, dp, &mut f);
+    }
+}
