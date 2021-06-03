4902a1b9144e21127e53d0d86a41202992881525
to-omer <toomerhs@gmail.com>
2021-06-04 01:52:27 +0900

    fix capture macros

--- crates/competitive/src/tools/capture.rs
+++ crates/competitive/src/tools/capture.rs
@@ -1,44 +1,36 @@
-#[macro_export]
-macro_rules! capture {
-    ([$($ca:tt)*], fn $name:ident($($arg:tt)*) -> $ret:ty $body:block) => {
-        capture!({}[$($ca)*,] fn $name($($arg)*) -> $ret $body)
-    };
-    ([$($ca:tt)*], fn $name:ident($($arg:tt)*) $body:block) => {
-        capture!({}[$($ca)*,] fn $name($($arg)*) -> () $body)
-    };
-    ({$(($g:ident, $ga:expr, $gt:ty))*}[] fn $name:ident($($a:ident: $at:ty),*) -> $ret:ty $body:block) => {
-        fn $name($($g: $gt,)* $($a: $at,)*) -> $ret {
-            #[allow(unused_macros)]
-            macro_rules! $name {
-                () => {
-                    |$($a),*| $name($($g,)* $($a,)*)
-                }
-            }
-            $body
-        }
-        #[allow(unused_mut)]
-        let mut $name = |$($a),*| $name($($ga,)* $($a,)*);
-    };
-    ({$($g:tt)*}[]fn $name:ident($($a:ident: $at:ty),*,) $($rest:tt)*) => {
-        capture!({$($g)*}[]fn $name($($a: $at),*) $($rest)*)
-    };
-    ({$($done:tt)*}[,] $($rest:tt)*) => {
-        capture!({$($done)*}[] $($rest)*)
-    };
-    ({$($done:tt)*}[$g:ident: &mut $gt:ty, $($rest:tt)*] $($info:tt)*) => {
-        capture!({$($done)* ($g, &mut $g, &mut $gt)}[$($rest)*] $($info)*)
-    };
-    ({$($done:tt)*}[$g:ident: &$gt:ty, $($rest:tt)*] $($info:tt)*) => {
-        capture!({$($done)* ($g, &$g, &$gt)}[$($rest)*] $($info)*)
-    };
-    ({$($done:tt)*}[$g:ident: $gt:ty, $($rest:tt)*] $($info:tt)*) => {
-        capture!({$($done)* ($g, $g, $gt)}[$($rest)*]$($info)*)
-    };
-    ({$($done:tt)*}[$g:ident, $($rest:tt)*] $($info:tt)*) => {
-        capture!({$($done)* ($g, $g, usize)}[$($rest)*]$($info)*)
-    };
-}
-
+/// Macro that returns a recursive function that automatically captures references
+/// and semi-automatically captures mutable references.
+///
+/// # Example
+/// ```
+/// # use competitive::crecurse;
+/// let mut res = 0usize;
+/// let coeff = 3usize;
+/// crecurse!(
+///     // (1) semi-automatically capture mutable reference (res: &mut usize)
+///     [res: usize],
+///     fn mul(x: usize, y: usize) {
+///         if y > 0 {
+///             if y % 2 == 1 {
+///                 // (2) automatically capture reference (coeff: &usize)
+///                 *res += coeff * x;
+///             }
+///             // (3) call macro to get captured version of the recursive function
+///             // internally, `mul!()` returns `|x, y| mul(x, y, res)`
+///             mul!()(x + x, y / 2);
+///         }
+///     }
+/// )(10, 19); // (4) macro returns captured version of the recursive function
+/// assert_eq!(res, coeff * 10 * 19);
+/// ```
+///
+/// # Syntax
+/// ```txt
+/// crecurse!(
+///     ([($ident: $type),*,?],)?
+///     fn $ident\(($ident: $type),*\) (-> $type)? $block
+/// )
+/// ```
 #[macro_export]
 macro_rules! crecurse {
     (
@@ -74,12 +66,55 @@ macro_rules! crecurse {
         }
     }};
     (fn $($rest:tt)*) => {
-        crecurse!([], fn $($rest)*)
+        $crate::crecurse!([], fn $($rest)*)
     };
     ([$($caps:tt)*], fn $func:ident ($($args:ident: $argsty:ty),*) $body:block) => {
-        crecurse!([$($caps)*], fn $func($($args: $argsty),*) -> () $body)
+        $crate::crecurse!([$($caps)*], fn $func($($args: $argsty),*) -> () $body)
     };
     ([$($caps:tt)*], fn $func:ident ($($args:ident: $argsty:ty),*,) $($rest:tt)*) => {
-        crecurse!([$($caps)*], fn $func($($args: $argsty),*) $($rest)*)
+        $crate::crecurse!([$($caps)*], fn $func($($args: $argsty),*) $($rest)*)
+    };
+}
+
+/// Automatic memorization for recursive functions.
+///
+/// This macro binds memorized version of the recursive functions to a local variable.
+/// The specification of the function declaration part is the same as [`crecurse`].
+///
+/// [`crecurse`]: crate::crecurse
+///
+/// # Example
+/// ```
+/// # use competitive::memorize;
+/// memorize!(
+///     fn comb(n: usize, r: usize) -> usize {
+///         if r > n {
+///             0
+///         } else if r == 0 || r == n {
+///             1
+///         } else {
+///             comb!()(n - 1, r) + comb!()(n - 1, r - 1)
+///         }
+///     }
+/// );
+/// assert_eq!(comb(10, 4), 210);
+/// ```
+#[macro_export]
+macro_rules! memorize {
+    (fn $name:ident ($($args:ident: $argsty:ty),*) -> $ret:ty $body:block) => {
+        let mut __memorize_cache = ::std::collections::HashMap::<($($argsty),*), $ret>::new();
+        #[allow(unused_mut)]
+        let mut $name = $crate::crecurse!(
+            [__memorize_cache: ::std::collections::HashMap::<($($argsty),*), $ret>],
+            fn $name ($($args: $argsty),*) -> $ret {
+                if let Some(__value) = __memorize_cache.get(&($($args),*)).cloned() {
+                    __value
+                } else {
+                    let __value = $body;
+                    __memorize_cache.insert(($($args),*), __value.clone());
+                    __value
+                }
+            }
+        );
     };
 }
