bc029ac40e12eeb9b25afdf05c4d9d99d76e6bec
to-omer <toomerhs@gmail.com>
2021-06-08 01:42:00 +0900

    fix capture macro

--- crates/competitive/src/tools/capture.rs
+++ crates/competitive/src/tools/capture.rs
@@ -15,9 +15,8 @@
 ///                 // (2) automatically capture reference (coeff: &usize)
 ///                 *res += coeff * x;
 ///             }
-///             // (3) call macro to get captured version of the recursive function
-///             // internally, `mul!()` returns `|x, y| mul(x, y, res)`
-///             mul!()(x + x, y / 2);
+///             // (3) call macro to recurse
+///             mul!(x + x, y / 2);
 ///         }
 ///     }
 /// )(10, 19); // (4) macro returns captured version of the recursive function
@@ -28,14 +27,18 @@
 /// ```txt
 /// crecurse!(
 ///     ([($ident: $type),*,?],)?
-///     fn $ident\(($ident: $type),*\) (-> $type)? $block
+///     fn $ident\(($ident: $type),*,?\) (-> $type)? $block
 /// )
 /// ```
 #[macro_export]
 macro_rules! crecurse {
+    (@macro_def ($dol:tt) $name:ident $($cargs:ident)*) => {
+        #[allow(unused_macros)]
+        macro_rules! $name { ($dol($dol args:expr),*) => { $name($dol($dol args,)* $($cargs,)* ) } }
+    };
     (
-        [$($cargs:ident: $cargsty:ty),*],
-        fn $func:ident ($($args:ident: $argsty:ty),*) -> $ret:ty $body:block
+        @inner [$($cargs:ident: $cargsty:ty),* $(,)?],
+        fn $func:ident ($($args:ident: $argsty:ty),* $(,)?) -> $ret:ty $body:block
     ) => {{
         fn call<F>(f: &F, $($args: $argsty,)* $($cargs: &mut $cargsty,)*) -> $ret
         where
@@ -51,29 +54,19 @@ macro_rules! crecurse {
         |$($args: $argsty,)*| -> $ret {
             call(
                 &|$func, $($args: $argsty,)* $($cargs: &mut $cargsty,)*| -> $ret {
-                    #[allow(unused_macros)]
-                    macro_rules! $func {
-                        () => {
-                            |$($args: $argsty,)*| -> $ret {
-                                $func($($args,)* $($cargs,)*)
-                            }
-                        }
-                    }
+                    $crate::crecurse!(@macro_def ($) $func $($cargs)*);
                     $body
                 },
                 $($args,)* $(&mut $cargs,)*
             )
         }
     }};
-    (fn $($rest:tt)*) => {
-        $crate::crecurse!([], fn $($rest)*)
+    (@inner [$($caps:tt)*], fn $func:ident ($($argstt:tt)*) $($rest:tt)*) => {
+        $crate::crecurse!(@inner [$($caps)*], fn $func ($($argstt)*) -> () $($rest)*)
     };
-    ([$($caps:tt)*], fn $func:ident ($($args:ident: $argsty:ty),*) $body:block) => {
-        $crate::crecurse!([$($caps)*], fn $func($($args: $argsty),*) -> () $body)
-    };
-    ([$($caps:tt)*], fn $func:ident ($($args:ident: $argsty:ty),*,) $($rest:tt)*) => {
-        $crate::crecurse!([$($caps)*], fn $func($($args: $argsty),*) $($rest)*)
-    };
+    ($([$($caps:tt)*],)? fn $func:ident ($($args:ident: $argsty:ty),* $(,)?) $($rest:tt)*) => {
+        $crate::crecurse!(@inner [$($($caps)*)?], fn $func ($($args: $argsty),*) $($rest)*)
+    }
 }
 
 /// Automatic memorization for recursive functions.
@@ -93,28 +86,41 @@ macro_rules! crecurse {
 ///         } else if r == 0 || r == n {
 ///             1
 ///         } else {
-///             comb!()(n - 1, r) + comb!()(n - 1, r - 1)
+///             comb!(n - 1, r) + comb!(n - 1, r - 1)
 ///         }
 ///     }
 /// );
-/// assert_eq!(comb(10, 4), 210);
+/// assert_eq!(comb(30, 12), 86493225);
 /// ```
 #[macro_export]
 macro_rules! memorize {
-    (fn $name:ident ($($args:ident: $argsty:ty),*) -> $ret:ty $body:block) => {
-        let mut __memorize_cache = ::std::collections::HashMap::<($($argsty),*), $ret>::new();
+    (
+        @inner [$map:ident, $Map:ty, $init:expr]
+        fn $name:ident ($($args:ident: $argsty:ty),* $(,)?) -> $ret:ty $body:block
+    ) => {
+        let mut $map: $Map = $init;
         #[allow(unused_mut)]
         let mut $name = $crate::crecurse!(
-            [__memorize_cache: ::std::collections::HashMap::<($($argsty),*), $ret>],
+            [$map: $Map],
             fn $name ($($args: $argsty),*) -> $ret {
-                if let Some(__value) = __memorize_cache.get(&($($args),*)).cloned() {
-                    __value
+                if let Some(value) = $map.get(&($($args,)*)).cloned() {
+                    value
                 } else {
-                    let __value = $body;
-                    __memorize_cache.insert(($($args),*), __value.clone());
-                    __value
+                    let value = $body;
+                    $map.insert(($($args,)*), value.clone());
+                    value
                 }
             }
         );
     };
+    (fn $name:ident ($($args:ident: $argsty:ty),* $(,)?) -> $ret:ty $body:block) => {
+        $crate::memorize!(
+            @inner [
+                __memorize_map,
+                ::std::collections::HashMap<($($argsty,)*), $ret>,
+                ::std::default::Default::default()
+            ]
+            fn $name ($($args: $argsty),*) -> $ret $body
+        );
+    }
 }
