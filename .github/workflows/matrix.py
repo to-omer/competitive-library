7da90c56861bf704511ff2b621eb2428a37f5756
to-omer <toomerhs@gmail.com>
2021-06-13 01:03:07 +0900

    fix matrix.py

--- .github/workflows/matrix.py
+++ .github/workflows/matrix.py
@@ -2,42 +2,53 @@ from argparse import ArgumentParser
 from itertools import islice
 from pathlib import Path
 from shutil import move
-from subprocess import run
+from subprocess import PIPE, run
 
 SIZE = 15
 
-verify_packages = ['aizu_online_judge', 'library_checker']
+verify_packages = ["aizu_online_judge library_checker"]
+
 
 def cargo_verify(package: str, name: str):
-    res = run(['cargo', 'test', '--package', package, '--release', name, '--', '--ignored', '--exact', '--nocapture'])
+    res = run(
+        [
+            f"cargo test --package {package} --release {name}"
+            " -- --ignored --exact --nocapture"
+        ]
+    )
     if res.returncode:
-        print(f'::error::verify failed `{name}`')
+        print(f"::error::verify failed `{name}`")
+
 
 def verify_list():
     for package in verify_packages:
-        command = ['cargo', 'test', '--package', package, '--quiet', '--release', '--', '--list', '--ignored']
-        res = run(command, capture_output=True)
+        command = [
+            f"cargo test --package {package} --quiet --release -- --list --ignored"
+        ]
+        res = run(command, stdout=PIPE)
         for s in res.stdout.splitlines():
-            yield (package, s.split()[0][:-1].decode('utf-8'))
+            yield (package, s.split()[0][:-1].decode("utf-8"))
+
 
 def arrange_artifacts():
-    res = run(['git', 'ls-files', '-o', '--exclude-standard', 'crates'], capture_output=True)
-    artifact = Path('artifact')
+    res = run(["git ls-files -o --exclude-standard crates"], stdout=PIPE)
+    artifact = Path("artifact")
     artifact.mkdir(exist_ok=True)
     for s in res.stdout.split():
-        target = artifact / Path(s.decode('utf-8'))
+        target = artifact / Path(s.decode("utf-8"))
         target.parent.mkdir(parents=True, exist_ok=True)
-        move(s.decode('utf-8'), target)
+        move(s.decode("utf-8"), target)
 
 
 def main():
     parser = ArgumentParser()
-    parser.add_argument('nth', type=int, nargs='?')
+    parser.add_argument("nth", type=int, nargs="?")
     args = parser.parse_args()
     for package, name in islice(verify_list(), args.nth, None, SIZE):
         cargo_verify(package, name)
 
     arrange_artifacts()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
