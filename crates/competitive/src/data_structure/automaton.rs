0c6fa9b6d2dc93769d57a6cddf7992f862458af3
to-omer <toomerhs@gmail.com>
2021-07-11 15:44:35 +0900

    improve automaton

--- crates/competitive/src/data_structure/automaton.rs
+++ crates/competitive/src/data_structure/automaton.rs
@@ -1,55 +1,153 @@
 use super::Monoid;
-use std::{cmp::Ordering, collections::HashMap, hash::Hash, marker::PhantomData};
+use std::{
+    borrow::Borrow, cmp::Ordering, collections::HashMap, hash::Hash, marker::PhantomData, mem::swap,
+};
 
 pub trait Automaton {
     type Alphabet;
     type State;
-    type Effect;
-
     fn initial(&self) -> Self::State;
-    fn next(
-        &self,
-        state: &Self::State,
-        alph: &Self::Alphabet,
-    ) -> Option<(Self::State, Self::Effect)>;
+    fn next(&self, state: &Self::State, alph: &Self::Alphabet) -> Option<Self::State>;
     fn accept(&self, state: &Self::State) -> bool;
+    fn dp<M>(&self, init: M::T) -> Automatondp<M, &Self>
+    where
+        Self: Sized,
+        Self::State: Eq + Hash,
+        M: Monoid,
+    {
+        Automatondp::new(self, init)
+    }
 }
 
-pub fn automaton_dp<M, A, I, F>(dfa: A, sigma: I, len: usize, mul: F, init: M::T) -> M::T
+impl<A> Automaton for &A
 where
     A: Automaton,
-    A::State: Eq + Hash,
-    M: Monoid,
-    I: Iterator<Item = A::Alphabet> + Clone,
-    F: Fn(&M::T, &A::Effect) -> M::T,
 {
-    let mut dp = HashMap::new();
-    let mut ndp = HashMap::new();
-    dp.insert(dfa.initial(), init);
-    for _ in 0..len {
-        for (state, value) in dp.drain() {
-            for alph in sigma.clone() {
-                if let Some((nstate, eff)) = dfa.next(&state, &alph) {
-                    let nvalue = mul(&value, &eff);
-                    ndp.entry(nstate)
+    type Alphabet = A::Alphabet;
+    type State = A::State;
+    fn initial(&self) -> Self::State {
+        A::initial(self)
+    }
+    fn next(&self, state: &Self::State, alph: &Self::Alphabet) -> Option<Self::State> {
+        A::next(self, state, alph)
+    }
+    fn accept(&self, state: &Self::State) -> bool {
+        A::accept(self, state)
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Automatondp<M, A>
+where
+    M: Monoid,
+    A: Automaton,
+    A::State: Eq + Hash,
+{
+    dfa: A,
+    pub dp: HashMap<A::State, M::T>,
+    ndp: HashMap<A::State, M::T>,
+}
+impl<M, A> Automatondp<M, A>
+where
+    M: Monoid,
+    A: Automaton,
+    A::State: Eq + Hash,
+{
+    pub fn new(dfa: A, init: M::T) -> Self {
+        let mut dp = HashMap::new();
+        let ndp = HashMap::new();
+        dp.insert(dfa.initial(), init);
+        Self { dfa, dp, ndp }
+    }
+    pub fn step<S, I, B>(&mut self, mut sigma: S)
+    where
+        S: FnMut() -> I,
+        I: IntoIterator<Item = B>,
+        B: Borrow<A::Alphabet>,
+    {
+        for (state, value) in self.dp.drain() {
+            for alph in sigma() {
+                if let Some(nstate) = self.dfa.next(&state, alph.borrow()) {
+                    self.ndp
+                        .entry(nstate)
+                        .and_modify(|acc| *acc = M::operate(acc, &value))
+                        .or_insert_with(|| value.clone());
+                }
+            }
+        }
+        swap(&mut self.dp, &mut self.ndp);
+    }
+    pub fn step_effect<S, I, B, F>(&mut self, mut sigma: S, mut effect: F)
+    where
+        S: FnMut() -> I,
+        I: IntoIterator<Item = B>,
+        B: Borrow<A::Alphabet>,
+        F: FnMut(&M::T, &A::State, &A::Alphabet, &A::State) -> M::T,
+    {
+        for (state, value) in self.dp.drain() {
+            for alph in sigma() {
+                if let Some(nstate) = self.dfa.next(&state, alph.borrow()) {
+                    let nvalue = effect(&value, &state, alph.borrow(), &nstate);
+                    self.ndp
+                        .entry(nstate)
                         .and_modify(|acc| *acc = M::operate(acc, &nvalue))
                         .or_insert(nvalue);
                 }
             }
         }
-        std::mem::swap(&mut dp, &mut ndp);
-        ndp.clear();
+        swap(&mut self.dp, &mut self.ndp);
     }
-    let mut acc = M::unit();
-    for (state, value) in dp.into_iter() {
-        if dfa.accept(&state) {
-            acc = M::operate(&acc, &value);
+    pub fn fold_accept(&self) -> M::T {
+        let mut acc = M::unit();
+        for (state, value) in self.dp.iter() {
+            if self.dfa.accept(state) {
+                acc = M::operate(&acc, value);
+            }
         }
+        acc
     }
-    acc
+    pub fn map_fold_accept<U, F>(&self, mut f: F) -> HashMap<U, M::T>
+    where
+        U: Eq + Hash,
+        F: FnMut(&A::State) -> U,
+    {
+        let mut map = HashMap::new();
+        for (state, value) in self.dp.iter() {
+            if self.dfa.accept(state) {
+                map.entry(f(state))
+                    .and_modify(|acc| *acc = M::operate(acc, value))
+                    .or_insert_with(|| value.clone());
+            }
+        }
+        map
+    }
+    pub fn run<S, I, B>(&mut self, mut sigma: S, len: usize) -> M::T
+    where
+        S: FnMut() -> I,
+        I: IntoIterator<Item = B>,
+        B: Borrow<A::Alphabet>,
+    {
+        for _ in 0..len {
+            self.step(&mut sigma);
+        }
+        self.fold_accept()
+    }
+    pub fn run_effect<S, I, B, F>(&mut self, mut sigma: S, len: usize, mut effect: F) -> M::T
+    where
+        S: FnMut() -> I,
+        I: IntoIterator<Item = B>,
+        B: Borrow<A::Alphabet>,
+        F: FnMut(&M::T, &A::State, &A::Alphabet, &A::State) -> M::T,
+    {
+        for _ in 0..len {
+            self.step_effect(&mut sigma, &mut effect);
+        }
+        self.fold_accept()
+    }
 }
 
-pub struct IntersectionAutomaton<X: Automaton, Y: Automaton>(X, Y);
+#[derive(Debug, Clone)]
+pub struct IntersectionAutomaton<X, Y>(pub X, pub Y);
 impl<A, X, Y> Automaton for IntersectionAutomaton<X, Y>
 where
     X: Automaton<Alphabet = A>,
@@ -57,17 +155,12 @@ where
 {
     type Alphabet = A;
     type State = (X::State, Y::State);
-    type Effect = (X::Effect, Y::Effect);
     fn initial(&self) -> Self::State {
         (self.0.initial(), self.1.initial())
     }
-    fn next(
-        &self,
-        state: &Self::State,
-        alph: &Self::Alphabet,
-    ) -> Option<(Self::State, Self::Effect)> {
+    fn next(&self, state: &Self::State, alph: &Self::Alphabet) -> Option<Self::State> {
         match (self.0.next(&state.0, alph), self.1.next(&state.1, alph)) {
-            (Some((s0, e0)), Some((s1, e1))) => Some(((s0, s1), (e0, e1))),
+            (Some(s0), Some(s1)) => Some((s0, s1)),
             _ => None,
         }
     }
@@ -76,7 +169,8 @@ where
     }
 }
 
-pub struct UnionAutomaton<X: Automaton, Y: Automaton>(X, Y);
+#[derive(Debug, Clone)]
+pub struct UnionAutomaton<X, Y>(pub X, pub Y);
 impl<A, X, Y> Automaton for UnionAutomaton<X, Y>
 where
     X: Automaton<Alphabet = A>,
@@ -84,17 +178,12 @@ where
 {
     type Alphabet = A;
     type State = (X::State, Y::State);
-    type Effect = (X::Effect, Y::Effect);
     fn initial(&self) -> Self::State {
         (self.0.initial(), self.1.initial())
     }
-    fn next(
-        &self,
-        state: &Self::State,
-        alph: &Self::Alphabet,
-    ) -> Option<(Self::State, Self::Effect)> {
+    fn next(&self, state: &Self::State, alph: &Self::Alphabet) -> Option<Self::State> {
         match (self.0.next(&state.0, alph), self.1.next(&state.1, alph)) {
-            (Some((s0, e0)), Some((s1, e1))) => Some(((s0, s1), (e0, e1))),
+            (Some(s0), Some(s1)) => Some((s0, s1)),
             _ => None,
         }
     }
@@ -103,24 +192,24 @@ where
     }
 }
 
-pub struct ProductAutomaton<X: Automaton, Y: Automaton>(X, Y);
-impl<X: Automaton, Y: Automaton> Automaton for ProductAutomaton<X, Y> {
+#[derive(Debug, Clone)]
+pub struct ProductAutomaton<X, Y>(pub X, pub Y);
+impl<X, Y> Automaton for ProductAutomaton<X, Y>
+where
+    X: Automaton,
+    Y: Automaton,
+{
     type Alphabet = (X::Alphabet, Y::Alphabet);
     type State = (X::State, Y::State);
-    type Effect = (X::Effect, Y::Effect);
     fn initial(&self) -> Self::State {
         (self.0.initial(), self.1.initial())
     }
-    fn next(
-        &self,
-        state: &Self::State,
-        alph: &Self::Alphabet,
-    ) -> Option<(Self::State, Self::Effect)> {
+    fn next(&self, state: &Self::State, alph: &Self::Alphabet) -> Option<Self::State> {
         match (
             self.0.next(&state.0, &alph.0),
             self.1.next(&state.1, &alph.1),
         ) {
-            (Some((s0, e0)), Some((s1, e1))) => Some(((s0, s1), (e0, e1))),
+            (Some(s0), Some(s1)) => Some((s0, s1)),
             _ => None,
         }
     }
@@ -129,112 +218,131 @@ impl<X: Automaton, Y: Automaton> Automaton for ProductAutomaton<X, Y> {
     }
 }
 
-pub struct LessThanAutomaton<'a, T: Ord> {
-    buf: &'a [T],
-    eq: bool,
+#[derive(Debug, Clone)]
+pub struct FunctionalAutomaton<A, S, F, G, H>
+where
+    F: Fn() -> S,
+    G: Fn(&S, &A) -> Option<S>,
+    H: Fn(&S) -> bool,
+{
+    fn_initial: F,
+    fn_next: G,
+    fn_accept: H,
+    _marker: PhantomData<fn() -> (A, S)>,
 }
-impl<'a, T: Ord> LessThanAutomaton<'a, T> {
-    pub fn new(buf: &'a [T], eq: bool) -> Self {
-        Self { buf, eq }
+impl<A, S, F, G, H> FunctionalAutomaton<A, S, F, G, H>
+where
+    F: Fn() -> S,
+    G: Fn(&S, &A) -> Option<S>,
+    H: Fn(&S) -> bool,
+{
+    pub fn new(fn_initial: F, fn_next: G, fn_accept: H) -> Self {
+        Self {
+            fn_initial,
+            fn_next,
+            fn_accept,
+            _marker: PhantomData,
+        }
     }
 }
-impl<T: Ord> Automaton for LessThanAutomaton<'_, T> {
+impl<A, S, F, G, H> Automaton for FunctionalAutomaton<A, S, F, G, H>
+where
+    F: Fn() -> S,
+    G: Fn(&S, &A) -> Option<S>,
+    H: Fn(&S) -> bool,
+{
+    type Alphabet = A;
+    type State = S;
+    fn initial(&self) -> Self::State {
+        (self.fn_initial)()
+    }
+    fn next(&self, state: &Self::State, alph: &Self::Alphabet) -> Option<Self::State> {
+        (self.fn_next)(state, alph)
+    }
+    fn accept(&self, state: &Self::State) -> bool {
+        (self.fn_accept)(state)
+    }
+}
+
+#[derive(Debug, Clone)]
+/// DFA to accept Less/Greater than (or equal to) the sequence
+pub struct LexicographicalAutomaton<'a, T> {
+    sequence: &'a [T],
+    ordering: Ordering,
+    equal: bool,
+}
+impl<'a, T> LexicographicalAutomaton<'a, T> {
+    pub fn less_than(sequence: &'a [T]) -> Self {
+        Self {
+            sequence,
+            ordering: Ordering::Less,
+            equal: false,
+        }
+    }
+    pub fn less_than_or_equal(sequence: &'a [T]) -> Self {
+        Self {
+            sequence,
+            ordering: Ordering::Less,
+            equal: true,
+        }
+    }
+    pub fn greater_than(sequence: &'a [T]) -> Self {
+        Self {
+            sequence,
+            ordering: Ordering::Greater,
+            equal: false,
+        }
+    }
+    pub fn greater_than_or_equal(sequence: &'a [T]) -> Self {
+        Self {
+            sequence,
+            ordering: Ordering::Greater,
+            equal: true,
+        }
+    }
+}
+impl<'a, T> Automaton for LexicographicalAutomaton<'a, T>
+where
+    T: Ord,
+{
     type Alphabet = T;
+    /// (next position of sequence, is equal)
     type State = (usize, bool);
-    type Effect = ();
     fn initial(&self) -> Self::State {
         (0, true)
     }
-    fn next(
-        &self,
-        state: &Self::State,
-        alph: &Self::Alphabet,
-    ) -> Option<(Self::State, Self::Effect)> {
-        self.buf
+    fn next(&self, state: &Self::State, alph: &Self::Alphabet) -> Option<Self::State> {
+        self.sequence
             .get(state.0)
             .and_then(|c| match (state.1, c.cmp(alph)) {
-                (true, Ordering::Equal) => Some(((state.0 + 1, true), ())),
-                (true, Ordering::Less) => None,
-                _ => Some(((state.0 + 1, false), ())),
+                (true, Ordering::Equal) => Some((state.0 + 1, true)),
+                (true, ord) if ord == self.ordering => None,
+                _ => Some((state.0 + 1, false)),
             })
     }
     fn accept(&self, state: &Self::State) -> bool {
-        self.eq || !state.1
+        self.equal || !state.1
     }
 }
 
-pub struct GreaterThanAutomaton<'a, T: Ord> {
-    buf: &'a [T],
-    eq: bool,
-}
-impl<'a, T: Ord> GreaterThanAutomaton<'a, T> {
-    pub fn new(buf: &'a [T], eq: bool) -> Self {
-        Self { buf, eq }
-    }
-}
-impl<T: Ord> Automaton for GreaterThanAutomaton<'_, T> {
-    type Alphabet = T;
-    type State = (usize, bool);
-    type Effect = ();
+#[derive(Debug, Clone)]
+pub struct MonoidalAutomaton<M>(PhantomData<fn() -> M>)
+where
+    M: Monoid;
+impl<M> Automaton for MonoidalAutomaton<M>
+where
+    M: Monoid,
+{
+    type Alphabet = M::T;
+    type State = M::T;
     fn initial(&self) -> Self::State {
-        (0, true)
+        M::unit()
     }
-    fn next(
-        &self,
-        state: &Self::State,
-        alph: &Self::Alphabet,
-    ) -> Option<(Self::State, Self::Effect)> {
-        self.buf
-            .get(state.0)
-            .and_then(|c| match (state.1, c.cmp(alph)) {
-                (true, Ordering::Equal) => Some(((state.0 + 1, true), ())),
-                (true, Ordering::Greater) => None,
-                _ => Some(((state.0 + 1, false), ())),
-            })
+    fn next(&self, state: &Self::State, alph: &Self::Alphabet) -> Option<Self::State> {
+        Some(M::operate(state, alph))
     }
-    fn accept(&self, state: &Self::State) -> bool {
-        self.eq || !state.1
-    }
-}
-
-pub struct ContainAutomaton<'a, T: Eq>(&'a T);
-impl<'a, T: Eq> Automaton for ContainAutomaton<'a, T> {
-    type Alphabet = T;
-    type State = bool;
-    type Effect = bool;
-    fn initial(&self) -> Self::State {
-        false
-    }
-    fn next(
-        &self,
-        state: &Self::State,
-        alph: &Self::Alphabet,
-    ) -> Option<(Self::State, Self::Effect)> {
-        Some((*state || self.0 == alph, *state ^ (self.0 == alph)))
-    }
-    fn accept(&self, state: &Self::State) -> bool {
-        *state
-    }
-}
-
-pub struct ContainCounterAutomaton<'a, T: Eq>(&'a T);
-impl<'a, T: Eq> Automaton for ContainCounterAutomaton<'a, T> {
-    type Alphabet = T;
-    type State = usize;
-    type Effect = usize;
-    fn initial(&self) -> Self::State {
-        0
-    }
-    fn next(
-        &self,
-        state: &Self::State,
-        alph: &Self::Alphabet,
-    ) -> Option<(Self::State, Self::Effect)> {
-        let nstate = *state + (self.0 == alph) as usize;
-        Some((nstate, nstate))
-    }
-    fn accept(&self, state: &Self::State) -> bool {
-        *state > 0
+    fn accept(&self, _state: &Self::State) -> bool {
+        true
     }
 }
 
@@ -253,16 +361,64 @@ impl<A> Default for AlwaysAcceptingAutomaton<A> {
 impl<A> Automaton for AlwaysAcceptingAutomaton<A> {
     type Alphabet = A;
     type State = ();
-    type Effect = ();
     fn initial(&self) -> Self::State {}
-    fn next(
-        &self,
-        _state: &Self::State,
-        _alph: &Self::Alphabet,
-    ) -> Option<(Self::State, Self::Effect)> {
-        Some(((), ()))
+    fn next(&self, _state: &Self::State, _alph: &Self::Alphabet) -> Option<Self::State> {
+        Some(())
     }
     fn accept(&self, _state: &Self::State) -> bool {
         true
     }
+}
+
+pub trait ToDigitSequence: Sized {
+    fn to_digit_sequence(&self, len: usize) -> Vec<Self>;
+    fn to_digit_sequence_radix(&self, radix: Self, len: usize) -> Vec<Self>;
+}
+
+macro_rules! impl_to_digit_sequence {
+    ($($t:ty)*) => {
+        $(impl ToDigitSequence for $t {
+            fn to_digit_sequence(&self, len: usize) -> Vec<$t> {
+                self.to_digit_sequence_radix(10, len)
+            }
+            fn to_digit_sequence_radix(&self, radix: Self, len: usize) -> Vec<$t> {
+                let mut x = *self;
+                let mut res: Vec<$t> = vec![0; len];
+                for r in res.iter_mut().rev() {
+                    if x == 0 {
+                        break;
+                    }
+                    *r = x % radix;
+                    x /= radix;
+                }
+                res
+            }
+        })*
+    };
+}
+impl_to_digit_sequence!(u8 u16 u32 u64 u128 usize);
+
+#[macro_export]
+macro_rules! automaton {
+    (@inner ($($t:tt)*))                              => { $crate::automaton!(@inner $($t)*) };
+    (@inner <= $e:expr)                               => { LexicographicalAutomaton::less_than_or_equal(&$e) };
+    (@inner >= $e:expr)                               => { LexicographicalAutomaton::greater_than_or_equal(&$e) };
+    (@inner < $e:expr)                                => { LexicographicalAutomaton::less_than(&$e) };
+    (@inner > $e:expr)                                => { LexicographicalAutomaton::greater_than(&$e) };
+    (@inner => $f:expr, $g:expr, $h:expr $(,)?)       => { FunctionalAutomaton::new($f, $g, $h) };
+    (@inner ($($h:tt)*) $($op:tt ($($t:tt)*))*)       => { $crate::automaton!(@union [] ($($h)*) $($op ($($t)*))*) };
+    (@inner $($t:tt)*)                                => { $($t)* };
+    (@union [] ($($h:tt)*) $($t:tt)*)                 => { $crate::automaton!(@union [($($h)*)] $($t)*) };
+    (@union [$($h:tt)*] | ($($x:tt)*) $($t:tt)*)      => { UnionAutomaton($crate::automaton!(@inner $($h)*), $crate::automaton!(@inner ($($x)*) $($t)*)) };
+    (@union [$($h:tt)*] $op:tt ($($x:tt)*) $($t:tt)*) => { $crate::automaton!(@union [$($h)* $op ($($x)*)] $($t)*) };
+    (@union [$($h:tt)*])                              => { $crate::automaton!(@inter [] $($h)*) };
+    (@inter [] ($($h:tt)*) $($t:tt)*)                 => { $crate::automaton!(@inter [($($h)*)] $($t)*) };
+    (@inter [$($h:tt)*] & ($($x:tt)*) $($t:tt)*)      => { IntersectionAutomaton($crate::automaton!(@inner $($h)*), $crate::automaton!(@inner ($($x)*) $($t)*)) };
+    (@inter [$($h:tt)*] $op:tt ($($x:tt)*) $($t:tt)*) => { $crate::automaton!(@inter [$($h)* $op ($($x)*)] $($t)*) };
+    (@inter [$($h:tt)*])                              => { $crate::automaton!(@prod [] $($h)*) };
+    (@prod [] ($($h:tt)*) $($t:tt)*)                  => { $crate::automaton!(@prod [($($h)*)] $($t)*) };
+    (@prod [$($h:tt)*] * ($($x:tt)*) $($t:tt)*)       => { ProductAutomaton($crate::automaton!(@inner $($h)*), $crate::automaton!(@inner ($($x)*) $($t)*)) };
+    (@prod [$($h:tt)*] $op:tt ($($x:tt)*) $($t:tt)*)  => { $crate::automaton!(@prod [$($h)* $op ($($x)*)] $($t)*) };
+    (@prod [$($h:tt)*])                               => { $crate::automaton!(@inner $($h)*) };
+    ($($t:tt)*)                                       => { $crate::automaton!(@inner $($t)*) };
 }
