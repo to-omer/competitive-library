c6df090c32ab01d98d784940d0229ae774f1dc6c
to-omer <toomerhs@gmail.com>
2021-10-18 18:20:02 +0900

    remove old BabyStepGiantStep

--- crates/competitive/src/math/nums.rs
+++ crates/competitive/src/math/nums.rs
@@ -15,43 +15,6 @@ pub fn binary_exponentiation<T: Clone + std::ops::MulAssign>(
     one
 }
 
-#[codesnip::entry("BabyStepGiantStep", include("modinv"))]
-#[derive(Clone, Debug)]
-pub struct BabyStepGiantStep {
-    p: u64,
-    r: u64,
-    baby: std::collections::HashMap<u64, u64>,
-}
-#[codesnip::entry("BabyStepGiantStep")]
-impl BabyStepGiantStep {
-    pub fn new(x: u64, p: u64) -> Self {
-        let m = (p as f32).sqrt() as u64 + 1;
-        let mut baby = std::collections::HashMap::new();
-        let mut a = 1;
-        for i in 0..m {
-            baby.entry(a).or_insert(i);
-            a = a * x % p;
-        }
-        BabyStepGiantStep {
-            p,
-            r: modinv(a as i64, p as i64) as u64,
-            baby,
-        }
-    }
-    // minimum i where x ** i = y mod p
-    pub fn solve(&self, y: u64) -> Option<u64> {
-        let m = self.baby.len() as u64;
-        let mut y = y;
-        for j in 0..m + 1 {
-            if let Some(i) = self.baby.get(&y) {
-                return Some(i + j * m);
-            }
-            y = y * self.r % self.p;
-        }
-        None
-    }
-}
-
 /// Sum of Floor of Linear
 ///
 /// $$\sum_{i=0}^{n-1}\left\lfloor\frac{a\times i+b}{m}\right\rfloor$$
