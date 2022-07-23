d965f6b6047b97bfbd900a41418fddf92d380811
to-omer <toomerhs@gmail.com>
2022-07-23 19:02:36 +0900

    faster trie

--- crates/competitive/src/data_structure/trie.rs
+++ crates/competitive/src/data_structure/trie.rs
@@ -1,72 +1,95 @@
 #[derive(Debug, Clone)]
 pub struct Trie {
-    child: Vec<Vec<usize>>,
-    accept: Vec<usize>,
+    child: Vec<usize>,
+    node_size: usize,
     char_size: usize,
 }
-
 impl Trie {
     pub fn new(char_size: usize) -> Self {
         Self {
-            child: vec![vec![0; char_size]],
-            accept: vec![0],
+            child: vec![0; char_size],
+            node_size: 1,
             char_size,
         }
     }
-    pub fn insert_at(
-        &mut self,
-        mut node: usize,
-        iter: impl IntoIterator<Item = usize>,
-    ) -> Vec<usize> {
+    pub fn with_capacity(char_size: usize, capacity: usize) -> Self {
+        let mut child = Vec::with_capacity(capacity * char_size);
+        child.resize_with(char_size, Default::default);
+        Self {
+            child,
+            node_size: 1,
+            char_size,
+        }
+    }
+    pub fn insert_once_at(&mut self, node: usize, ch: usize) -> usize {
+        let index = node * self.char_size + ch;
+        if self.child[index] == 0 {
+            self.child[index] = self.node_size;
+            self.child
+                .resize_with(self.child.len() + self.char_size, Default::default);
+            self.node_size += 1;
+        }
+        self.child[index]
+    }
+    pub fn insert_at<I>(&mut self, mut node: usize, iter: I) -> Vec<usize>
+    where
+        I: IntoIterator<Item = usize>,
+    {
         let mut path = Vec::new();
         for ch in iter.into_iter() {
             path.push(node);
-            if self.child[node][ch] == 0 {
-                self.child[node][ch] = self.child.len();
-                self.child.push(vec![0; self.char_size]);
-                self.accept.push(0);
-            }
-            node = self.child[node][ch];
+            node = self.insert_once_at(node, ch);
         }
         path.push(node);
-        self.accept[node] += 1;
         path
     }
-    pub fn insert(&mut self, iter: impl IntoIterator<Item = usize>) -> Vec<usize> {
+    pub fn insert<I>(&mut self, iter: I) -> Vec<usize>
+    where
+        I: IntoIterator<Item = usize>,
+    {
         self.insert_at(0, iter)
     }
-    pub fn find_at(
-        &self,
-        mut node: usize,
-        iter: impl IntoIterator<Item = usize>,
-    ) -> Result<usize, usize> {
+    pub fn find_at<I>(&self, mut node: usize, iter: I) -> Result<usize, usize>
+    where
+        I: IntoIterator<Item = usize>,
+    {
         for ch in iter.into_iter() {
-            if let Some(v) = self.child.get(node) {
-                node = v[ch];
+            if let Some(&nnode) = self.child.get(node * self.char_size + ch) {
+                node = nnode;
             } else {
                 return Err(node);
             }
         }
         Ok(node)
     }
-    pub fn find(&self, iter: impl IntoIterator<Item = usize>) -> Result<usize, usize> {
+    pub fn find<I>(&self, iter: I) -> Result<usize, usize>
+    where
+        I: IntoIterator<Item = usize>,
+    {
         self.find_at(0, iter)
     }
     pub fn next_node(&self, node: usize, ch: usize) -> Option<usize> {
-        if self.child[node][ch] == 0 {
+        let index = node * self.char_size + ch;
+        if self.child[index] == 0 {
             None
         } else {
-            Some(self.child[node][ch])
+            Some(self.child[index])
         }
     }
-    pub fn count(&self, node: usize) -> usize {
-        self.accept[node]
+    pub fn node_size(&self) -> usize {
+        self.node_size
     }
-    pub fn next_count(&self, node: usize, ch: usize) -> usize {
-        if let Some(node) = self.next_node(node, ch) {
-            self.count(node)
-        } else {
-            0
+    pub fn edges(&self) -> Vec<(usize, usize)> {
+        let mut edges = Vec::with_capacity(self.node_size - 1);
+        let mut stack = vec![0usize];
+        while let Some(node) = stack.pop() {
+            for ch in (0..self.char_size).rev() {
+                if let Some(nnode) = self.next_node(node, ch) {
+                    edges.push((node, nnode));
+                    stack.push(nnode);
+                }
+            }
         }
+        edges
     }
 }
