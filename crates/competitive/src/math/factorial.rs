0f33a5e6e4e9c2bfa25526efd30c97db2a83a2f0
to-omer <toomerhs@gmail.com>
2023-01-07 02:08:31 +0900

    improve SmallModMemorizedFactorial::combination

--- crates/competitive/src/math/factorial.rs
+++ crates/competitive/src/math/factorial.rs
@@ -1,3 +1,4 @@
+use super::prime_factors;
 use crate::num::{MInt, MIntConvert, One, Zero};
 
 #[codesnip::entry("factorial", include("MIntBase"))]
@@ -61,13 +62,17 @@ where
     }
 }
 
-#[codesnip::entry("SmallModMemorizedFactorial", include("MIntBase"))]
+#[codesnip::entry("SmallModMemorizedFactorial", include("MIntBase", "prime_factors"))]
 #[derive(Clone, Debug)]
 pub struct SmallModMemorizedFactorial<M>
 where
     M: MIntConvert<usize>,
 {
+    p: u32,
+    c: u32,
     fact: Vec<MInt<M>>,
+    inv_fact: Vec<MInt<M>>,
+    pow: Vec<MInt<M>>,
 }
 #[codesnip::entry("SmallModMemorizedFactorial")]
 impl<M> Default for SmallModMemorizedFactorial<M>
@@ -75,12 +80,41 @@ where
     M: MIntConvert<usize>,
 {
     fn default() -> Self {
-        let p = M::mod_into();
-        let mut fact = vec![MInt::<M>::one(); p];
-        for i in 1..p {
-            fact[i] = fact[i - 1] * MInt::<M>::from(i);
+        let m = M::mod_into();
+        let pf = prime_factors(m as _);
+        assert!(pf.len() <= 1);
+        let p = pf[0].0 as u32;
+        let c = pf[0].1;
+        let mut fact = vec![MInt::one(); m];
+        let mut inv_fact = vec![MInt::one(); m];
+        let mut pow = vec![MInt::one(); c as usize];
+        for i in 2..m {
+            fact[i] = fact[i - 1]
+                * if i as u32 % p != 0 {
+                    MInt::from(i)
+                } else {
+                    MInt::one()
+                };
         }
-        Self { fact }
+        inv_fact[m - 1] = fact[m - 1].inv();
+        for i in (3..m).rev() {
+            inv_fact[i - 1] = inv_fact[i]
+                * if i as u32 % p != 0 {
+                    MInt::from(i)
+                } else {
+                    MInt::one()
+                };
+        }
+        for i in 1..c as usize {
+            pow[i] = pow[i - 1] * MInt::from(p as usize);
+        }
+        Self {
+            p,
+            c,
+            fact,
+            inv_fact,
+            pow,
+        }
     }
 }
 #[codesnip::entry("SmallModMemorizedFactorial")]
@@ -91,9 +125,9 @@ where
     pub fn new() -> Self {
         Default::default()
     }
-    /// n! = a * p^e
+    /// n! = a * p^e, c==1
     pub fn factorial(&self, n: usize) -> (MInt<M>, usize) {
-        let p = M::mod_into();
+        let p = self.p as usize;
         if n == 0 {
             (MInt::<M>::one(), 0)
         } else {
@@ -106,16 +140,41 @@ where
             }
         }
     }
-    pub fn combination(&self, n: usize, r: usize) -> MInt<M> {
-        if r <= n {
-            let (a1, e1) = self.factorial(n);
-            let (a2, e2) = self.factorial(r);
-            let (a3, e3) = self.factorial(n - r);
-            if e1 <= e2 + e3 {
-                return a1 * (a2 * a3).inv();
-            }
+    pub fn combination(&self, mut n: usize, mut r: usize) -> MInt<M> {
+        if r > n {
+            return MInt::<M>::zero();
         }
-        MInt::<M>::zero()
+        if self.p == 2 && self.c == 1 {
+            return MInt::from(((!n & r) == 0) as usize);
+        }
+        let mut k = n - r;
+        let m = M::mod_into();
+        let p = self.p as usize;
+        let cnte = |mut x: usize| {
+            let mut e = 0usize;
+            while x > 0 {
+                e += x;
+                x /= p;
+            }
+            e
+        };
+        let e0 = cnte(n / p) - cnte(r / p) - cnte(k / p);
+        if e0 >= self.c as usize {
+            return MInt::<M>::zero();
+        }
+        let mut res = self.pow[e0];
+        if (self.p > 2 && self.c >= 2 || self.c == 2)
+            && (cnte(n / m) - cnte(r / m) - cnte(k / m)) % 2 == 1
+        {
+            res = -res;
+        }
+        while n > 0 {
+            res *= self.fact[n % m] * self.inv_fact[r % m] * self.inv_fact[k % m];
+            n /= p;
+            r /= p;
+            k /= p;
+        }
+        res
     }
 }
 
@@ -209,6 +268,8 @@ mod tests {
             let k = rng.gen(0..=n);
             let x = fact.factorial(n).1 - fact.factorial(k).1 - fact.factorial(n - k).1;
             assert_eq!(x == 0, (n & k) == k);
+            let x = fact.combination(n, k);
+            assert_eq!(x.is_one(), (n & k) == k);
         }
     }
 }
