18816be9feac03db4e3734c7394e51e5728a6b50
to-omer <48119218+to-omer@users.noreply.github.com>
2023-08-29 00:48:57 +0900

    use const for montgomery

--- crates/competitive/src/num/mint/montgomery.rs
+++ crates/competitive/src/num/mint/montgomery.rs
@@ -7,7 +7,7 @@ where
     type Inner = u32;
     #[inline]
     fn get_mod() -> Self::Inner {
-        <Self as MontgomeryReduction32>::get_mod()
+        <Self as MontgomeryReduction32>::MOD
     }
     #[inline]
     fn mod_zero() -> Self::Inner {
@@ -15,7 +15,7 @@ where
     }
     #[inline]
     fn mod_one() -> Self::Inner {
-        Self::n1()
+        Self::N1
     }
     #[inline]
     fn mod_add(x: Self::Inner, y: Self::Inner) -> Self::Inner {
@@ -62,7 +62,7 @@ where
             std::mem::swap(&mut x, &mut u);
             std::mem::swap(&mut b, &mut a);
         }
-        Self::reduce((if x < 0 { x + p } else { x }) as u64 * Self::n3() as u64)
+        Self::reduce((if x < 0 { x + p } else { x }) as u64 * Self::N3 as u64)
     }
 }
 impl<M> MIntConvert<u32> for M
@@ -71,7 +71,7 @@ where
 {
     #[inline]
     fn from(x: u32) -> Self::Inner {
-        Self::reduce(x as u64 * Self::n2() as u64)
+        Self::reduce(x as u64 * Self::N2 as u64)
     }
     #[inline]
     fn into(x: Self::Inner) -> u32 {
@@ -88,7 +88,7 @@ where
 {
     #[inline]
     fn from(x: u64) -> Self::Inner {
-        Self::reduce(x % Self::get_mod() as u64 * Self::n2() as u64)
+        Self::reduce(x % Self::get_mod() as u64 * Self::N2 as u64)
     }
     #[inline]
     fn into(x: Self::Inner) -> u64 {
@@ -105,7 +105,7 @@ where
 {
     #[inline]
     fn from(x: usize) -> Self::Inner {
-        Self::reduce(x as u64 % Self::get_mod() as u64 * Self::n2() as u64)
+        Self::reduce(x as u64 % Self::get_mod() as u64 * Self::N2 as u64)
     }
     #[inline]
     fn into(x: Self::Inner) -> usize {
@@ -128,7 +128,7 @@ where
         } else {
             x as u64
         };
-        Self::reduce(x * Self::n2() as u64)
+        Self::reduce(x * Self::N2 as u64)
     }
     #[inline]
     fn into(x: Self::Inner) -> i32 {
@@ -151,7 +151,7 @@ where
         } else {
             x as u64
         };
-        Self::reduce(x * Self::n2() as u64)
+        Self::reduce(x * Self::N2 as u64)
     }
     #[inline]
     fn into(x: Self::Inner) -> i64 {
@@ -174,7 +174,7 @@ where
         } else {
             x as u64
         };
-        Self::reduce(x * Self::n2() as u64)
+        Self::reduce(x * Self::N2 as u64)
     }
     #[inline]
     fn into(x: Self::Inner) -> isize {
@@ -188,31 +188,33 @@ where
 /// m is prime, n = 2^32
 pub trait MontgomeryReduction32 {
     /// m
-    fn get_mod() -> u32;
+    const MOD: u32;
     /// (-m)^{-1} mod n
-    fn r() -> u32 {
-        let m = Self::get_mod();
+    const R: u32 = {
+        let m = Self::MOD;
         let mut r = 0;
         let mut t = 0;
-        for i in 0..32 {
+        let mut i = 0;
+        while i < 32 {
             if t % 2 == 0 {
                 t += m;
                 r += 1 << i;
             }
             t /= 2;
+            i += 1;
         }
         r
-    }
+    };
     /// n^1 mod m
-    fn n1() -> u32;
+    const N1: u32 = ((1u64 << 32) % Self::MOD as u64) as _;
     /// n^2 mod m
-    fn n2() -> u32;
+    const N2: u32 = (Self::N1 as u64 * Self::N1 as u64 % Self::MOD as u64) as _;
     /// n^3 mod m
-    fn n3() -> u32;
+    const N3: u32 = (Self::N1 as u64 * Self::N2 as u64 % Self::MOD as u64) as _;
     /// n^{-1}x = (x + (xr mod n)m) / n
     fn reduce(x: u64) -> u32 {
-        let m: u32 = Self::get_mod();
-        let r = Self::r();
+        let m: u32 = Self::MOD;
+        let r = Self::R;
         let mut x = ((x + r.wrapping_mul(x as u32) as u64 * m as u64) >> 32) as u32;
         if x >= m {
             x -= m;
@@ -221,122 +223,22 @@ pub trait MontgomeryReduction32 {
     }
 }
 macro_rules! define_montgomery_reduction_32 {
-    ($([$name:ident, $m:expr, $r:expr, $n1:expr, $n2:expr, $n3:expr, $mint_name:ident $(,)?]),* $(,)?) => {
+    ($([$name:ident, $m:expr, $mint_name:ident $(,)?]),* $(,)?) => {
         $(
             pub enum $name {}
             impl MontgomeryReduction32 for $name {
-                #[inline]
-                fn get_mod() -> u32 {
-                    $m
-                }
-                #[inline]
-                fn r() -> u32 {
-                    $r
-                }
-                #[inline]
-                fn n1() -> u32 {
-                    $n1
-                }
-                #[inline]
-                fn n2() -> u32 {
-                    $n2
-                }
-                #[inline]
-                fn n3() -> u32 {
-                    $n3
-                }
+                const MOD: u32 = $m;
             }
             pub type $mint_name = MInt<$name>;
         )*
     };
 }
 define_montgomery_reduction_32!(
-    [
-        Modulo998244353,
-        998_244_353,
-        998_244_351,
-        301_989_884,
-        932_051_910,
-        679_058_953,
-        MInt998244353,
-    ],
-    [
-        Modulo2113929217,
-        2_113_929_217,
-        2_113_929_215,
-        67_108_862,
-        2_111_798_781,
-        239_209_529,
-        MInt2113929217,
-    ],
-    [
-        Modulo1811939329,
-        1_811_939_329,
-        1_811_939_327,
-        671_088_638,
-        959_408_210,
-        1_483_943_592,
-        MInt1811939329,
-    ],
-    [
-        Modulo2013265921,
-        2_013_265_921,
-        2_013_265_919,
-        268_435_454,
-        1_172_168_163,
-        317_946_875,
-        MInt2013265921,
-    ],
+    [Modulo998244353, 998_244_353, MInt998244353],
+    [Modulo2113929217, 2_113_929_217, MInt2113929217],
+    [Modulo1811939329, 1_811_939_329, MInt1811939329],
+    [Modulo2013265921, 2_013_265_921, MInt2013265921],
 );
-
-// #[test]
-#[allow(dead_code)]
-#[codesnip::skip]
-fn culculate_montgomery() {
-    use crate::math::modinv;
-    let m: u64 = 998_244_353;
-    println!("m = {}", m);
-    let n = 1u64 << 32;
-    println!("n = {}", n);
-    // n^{-1} mod m
-    let ninv = modinv(n, m);
-    println!("n^{{-1}} = {}", ninv);
-    // r = (-m)^{-1} mod n
-    let r = modinv(n - m, n);
-    println!("r = {}", r);
-    let get_r = || {
-        let mut r = 0;
-        let mut n = n;
-        let mut i = 1;
-        let mut t = 0;
-        while n > 1 {
-            if t % 2 == 0 {
-                t += m;
-                r += i;
-            }
-            t /= 2;
-            n /= 2;
-            i *= 2;
-        }
-        r
-    };
-    assert_eq!((get_r() * m + 1) & (n - 1), 0);
-    assert_eq!((r * m + 1) & (n - 1), 0);
-    assert_eq!(r, get_r());
-    let n1 = n % m;
-    println!("n^1 = {}", n1);
-    let n2 = n1 * n1 % m;
-    println!("n^2 = {}", n2);
-    let n3 = n1 * n2 % m;
-    println!("n^3 = {}", n3);
-    // (x + (xr mod n)m) / n
-    // r < n
-    // xr < xn
-    // xr mod n < n
-    // (xr mod n)m < mn
-    // x + (xr mod n)m < mn + x
-    // (x + (xr mod n)m)/n < m + x/n
-}
 
 #[cfg(test)]
 mod tests {
@@ -351,14 +253,14 @@ mod tests {
         assert_eq!(0, MInt998244353::zero().inner());
         assert_eq!(1, MInt998244353::one().inner());
         assert_eq!(
-            Modulo998244353::reduce(Modulo998244353::n3() as u64),
-            Modulo998244353::n2()
+            Modulo998244353::reduce(Modulo998244353::N3 as u64),
+            Modulo998244353::N2
         );
         assert_eq!(
-            Modulo998244353::reduce(Modulo998244353::n2() as u64),
-            Modulo998244353::n1()
+            Modulo998244353::reduce(Modulo998244353::N2 as u64),
+            Modulo998244353::N1
         );
-        assert_eq!(Modulo998244353::reduce(Modulo998244353::n1() as u64), 1);
+        assert_eq!(Modulo998244353::reduce(Modulo998244353::N1 as u64), 1);
         for _ in 0..Q {
             let x = rng.gen(..MInt998244353::get_mod());
             assert_eq!(x, MInt998244353::new(x).inner());
