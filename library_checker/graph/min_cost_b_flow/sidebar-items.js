e6cd04150984217c743a425c52aba2ce0b33e40f
to-omer <to-omer@users.noreply.github.com>
2025-09-15 07:34:15 +0000

    deploy: 5676c2626bce98bee3697e86b510fa5d9d031ce3

---
+++ library_checker/graph/min_cost_b_flow/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":["min_cost_b_flow"]};
--- library_checker/graph/sidebar-items.js
+++ library_checker/graph/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":["bipartitematching","directedmst","lca","scc"]};
+window.SIDEBAR_ITEMS = {"mod":["assignment","bipartitematching","directedmst","lca","min_cost_b_flow","scc"]};
