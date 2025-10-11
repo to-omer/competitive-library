f7b5fd9d7fc0b2ffab5ed07963e36ad57b52ae6c
to-omer <48119218+to-omer@users.noreply.github.com>
2025-10-11 18:05:33 +0900

    add log_of_formal_power_series_sparse

---
+++ crates/library_checker/src/polynomial/log_of_formal_power_series_sparse.rs
@@ -0,0 +1,18 @@
+use competitive::prelude::*;
+#[doc(no_inline)]
+pub use competitive::{math::Fps998244353, num::Zero as _, num::montgomery::MInt998244353};
+
+#[verify::library_checker("log_of_formal_power_series_sparse")]
+pub fn log_of_formal_power_series_sparse(reader: impl Read, mut writer: impl Write) {
+    let s = read_all_unchecked(reader);
+    let mut scanner = Scanner::new(&s);
+    scan!(scanner, n, k);
+    let mut a = vec![MInt998244353::zero(); n];
+    for _ in 0..k {
+        scan!(scanner, i, a_i: MInt998244353);
+        a[i] = a_i;
+    }
+    let f = Fps998244353::from_vec(a);
+    let g = f.log(n);
+    iter_print!(writer, @it g.data);
+}
