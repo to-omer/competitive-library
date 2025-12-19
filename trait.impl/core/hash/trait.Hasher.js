86743fd1a442a41998be3d75e29454dd4e2d1c3f
to-omer <to-omer@users.noreply.github.com>
2025-12-19 01:03:11 +0000

    deploy: 429319ab6661c95bf33cc0a3e50a9986b9ac058c

--- trait.impl/core/hash/trait.Hasher.js
+++ trait.impl/core/hash/trait.Hasher.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["competitive",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hasher.html\" title=\"trait core::hash::Hasher\">Hasher</a> for <a class=\"struct\" href=\"competitive/data_structure/struct.FibonacciHasheru32.html\" title=\"struct competitive::data_structure::FibonacciHasheru32\">FibonacciHasheru32</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hasher.html\" title=\"trait core::hash::Hasher\">Hasher</a> for <a class=\"struct\" href=\"competitive/data_structure/struct.FibonacciHasheru64.html\" title=\"struct competitive::data_structure::FibonacciHasheru64\">FibonacciHasheru64</a>"]]]]);
+    const implementors = Object.fromEntries([["competitive",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hasher.html\" title=\"trait core::hash::Hasher\">Hasher</a> for <a class=\"struct\" href=\"competitive/data_structure/struct.FibonacciHasheru32.html\" title=\"struct competitive::data_structure::FibonacciHasheru32\">FibonacciHasheru32</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hasher.html\" title=\"trait core::hash::Hasher\">Hasher</a> for <a class=\"struct\" href=\"competitive/data_structure/struct.FibonacciHasheru64.html\" title=\"struct competitive::data_structure::FibonacciHasheru64\">FibonacciHasheru64</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[665]}
+//{"start":59,"fragment_lengths":[669]}
