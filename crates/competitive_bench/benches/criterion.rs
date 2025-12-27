28db429c258f09603b13d026b96589c15f25a16d
to-omer <48119218+to-omer@users.noreply.github.com>
2025-12-27 20:01:32 +0900

    reduce bench time

--- crates/competitive_bench/benches/criterion.rs
+++ crates/competitive_bench/benches/criterion.rs
@@ -10,8 +10,8 @@ mod special_ring;
 criterion_group!(
     name = small_benches;
     config = Criterion::default()
-        .warm_up_time(Duration::from_secs(1))
-        .measurement_time(Duration::from_secs(1));
+        .warm_up_time(Duration::from_millis(500))
+        .measurement_time(Duration::from_millis(500));
     targets =
         barrett_reduction::bench_barrett_reduction_u32,
         barrett_reduction::bench_barrett_reduction_u64,
