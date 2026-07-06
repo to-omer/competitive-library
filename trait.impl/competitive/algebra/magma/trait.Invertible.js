4fdf250db8388d1b96c1df1e9b5783a998c14b3a
to-omer <to-omer@users.noreply.github.com>
2026-07-06 14:56:35 +0000

    deploy: 5d4b5999010d2654835b5930d876066a27a1503f

--- trait.impl/competitive/algebra/magma/trait.Invertible.js
+++ trait.impl/competitive/algebra/magma/trait.Invertible.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["competitive",[]],["library_checker",[["impl Invertible for <a class=\"struct\" href=\"library_checker/data_structure/unionfind_with_potential_non_commutative_group/struct.Sl2.html\" title=\"struct library_checker::data_structure::unionfind_with_potential_non_commutative_group::Sl2\">Sl2</a>",0]]]]);
+    const implementors = Object.fromEntries([["competitive",[]],["library_checker",[["impl <a class=\"trait\" href=\"https://to-omer.github.io/competitive-library/competitive/algebra/magma/trait.Invertible.html\" title=\"trait competitive::algebra::magma::Invertible\">Invertible</a> for <a class=\"struct\" href=\"library_checker/data_structure/unionfind_with_potential_non_commutative_group/struct.Sl2.html\" title=\"struct library_checker::data_structure::unionfind_with_potential_non_commutative_group::Sl2\">Sl2</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[18,281]}
+//{"start":59,"fragment_lengths":[18,463]}
--- trait.impl/competitive/tree/static_top_tree/trait.MonoidCluster.js
+++ trait.impl/competitive/tree/static_top_tree/trait.MonoidCluster.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["library_checker",[["impl MonoidCluster for <a class=\"struct\" href=\"library_checker/tree/point_set_tree_path_composite_sum/struct.Dp.html\" title=\"struct library_checker::tree::point_set_tree_path_composite_sum::Dp\">Dp</a>",0],["impl MonoidCluster for <a class=\"struct\" href=\"library_checker/tree/point_set_tree_path_composite_sum_fixed_root/struct.Dp.html\" title=\"struct library_checker::tree::point_set_tree_path_composite_sum_fixed_root::Dp\">Dp</a>",0]]]]);
+    const implementors = Object.fromEntries([["library_checker",[["impl <a class=\"trait\" href=\"https://to-omer.github.io/competitive-library/competitive/tree/static_top_tree/trait.MonoidCluster.html\" title=\"trait competitive::tree::static_top_tree::MonoidCluster\">MonoidCluster</a> for <a class=\"struct\" href=\"library_checker/tree/point_set_tree_path_composite_sum/struct.Dp.html\" title=\"struct library_checker::tree::point_set_tree_path_composite_sum::Dp\">Dp</a>",0],["impl <a class=\"trait\" href=\"https://to-omer.github.io/competitive-library/competitive/tree/static_top_tree/trait.MonoidCluster.html\" title=\"trait competitive::tree::static_top_tree::MonoidCluster\">MonoidCluster</a> for <a class=\"struct\" href=\"library_checker/tree/point_set_tree_path_composite_sum_fixed_root/struct.Dp.html\" title=\"struct library_checker::tree::point_set_tree_path_composite_sum_fixed_root::Dp\">Dp</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[469]}
+//{"start":59,"fragment_lengths":[873]}
