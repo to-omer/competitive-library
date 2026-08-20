4f8b95225444145bd1ac18662a3fa310974b6804
to-omer <48119218+to-omer@users.noreply.github.com>
2026-08-21 01:56:20 +0900

    speed up vertex_add_subtree_sum

--- crates/library_checker/src/tree/vertex_add_subtree_sum.rs
+++ crates/library_checker/src/tree/vertex_add_subtree_sum.rs
@@ -1,6 +1,7 @@
 use competitive::prelude::*;
 use competitive::{
     algebra::AdditiveOperation, data_structure::SegmentTree, graph::UndirectedSparseGraph,
+    tree::XorLinkedRootedTree,
 };
 
 competitive::define_enum_scan! {
@@ -15,18 +16,18 @@ pub fn vertex_add_subtree_sum(reader: impl Read, mut writer: impl Write) {
     let s = read_all_unchecked(reader);
     let mut scanner = Scanner::new(&s);
     scan!(scanner, n, q, a: [u64; n], p: [usize]);
-    let edges = p.take(n - 1).enumerate().map(|(i, p)| (i + 1, p)).collect();
-    let tree = UndirectedSparseGraph::from_edges(n, edges);
-    let (et, b) = tree.subtree_euler_tour_builder(0).build_with_rearrange(&a);
+    let edges = p.take(n - 1).enumerate().map(|(i, p)| (i + 1, p));
+    let tree = XorLinkedRootedTree::builder(n)
+        .with_dfs_preorder()
+        .build(0, edges);
+    let b: Vec<_> = tree.dfs_order().iter().map(|&v| a[v]).collect();
     let mut seg = SegmentTree::<AdditiveOperation<_>>::from_vec(b);
     for _ in 0..q {
         scan!(scanner, query: Query);
         match query {
-            Query::Add { u, x } => {
-                et.update(u, x, |k, x| seg.update(k, x));
-            }
+            Query::Add { u, x } => seg.update(tree.dfs_index(u), x),
             Query::Sum { u } => {
-                writeln!(writer, "{}", et.fold(u, |r| seg.fold(r))).ok();
+                writeln!(writer, "{}", seg.fold(tree.subtree_range(u))).ok();
             }
         }
     }
